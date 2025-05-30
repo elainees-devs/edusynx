f1f21fa497068e25ac237e44db4a34b94c718946
elainees-devs <elainees.devs@gmail.com>
2025-05-30 18:17:51 +0300

    feat(types): export student, finance, and enums from index

--- server/src/types/index.ts
+++ server/src/types/index.ts
@@ -1,4 +1,8 @@
 //src/types/index.ts
 export * from "./school/school";
 export * from "./people/user";
+export * from "./people/student";
+export * from "./finance/finance";
+export * from "./enum/enum";
+
 
