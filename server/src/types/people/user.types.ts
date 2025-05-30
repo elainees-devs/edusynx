c3e61f24598b959821b2f5b328eef45d946be9fd
elainees-devs <elainees.devs@gmail.com>
2025-05-30 19:00:28 +0300

    refactor(types): extend IBaseUser from BaseDocument and update imports to use explicit *.types paths

---
+++ server/src/types/people/user.types.ts
@@ -0,0 +1,39 @@
+// src/types/people/user.ts
+import { Types } from "mongoose";
+import { IClass, ISchool } from "../school/school.types";
+import { UserRole } from "../enum/enum";
+import { BaseDocument } from "../common/base.types";
+
+export interface IBaseUser extends BaseDocument {
+  school: Types.ObjectId | ISchool;
+  firstName: string;
+  middleName: string;
+  lastName: string;
+  primaryEmail: string;
+  secondaryEmail?: string;
+  primaryPhoneNumber: string;
+  secondaryPhoneNumber?: string;
+  hashedPassword: string;
+  nationality: string;
+  avatarUrl?: string;
+  isActive: boolean;
+  lastLogin?: Date;
+  isLocked: boolean;
+  passwordChangedAt?: Date;
+  isTwoFactorEnabled: boolean;
+  role: UserRole;
+}
+
+
+export interface ITeacherUser extends IBaseUser {
+  role: UserRole.Teacher;
+  teacherId?: string;
+  isClassTeacher?: boolean;
+  class: Types.ObjectId | IClass;
+  teachingSubjects?: string[];
+}
+
+export interface IGuardianUser extends IBaseUser {
+  role: UserRole.Guardian;
+  familyNumber: number;
+}
