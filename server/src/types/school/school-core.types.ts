a92a3d4f26bd143a3a213f8d24502d4c3660b9e6
elainees-devs <elainees.devs@gmail.com>
2025-05-30 20:07:44 +0300

    feat(types): split activity-based types into school-activity.types.ts

---
+++ server/src/types/school/school-core.types.ts
@@ -0,0 +1,36 @@
+//src/types.school/school-core.types.ts
+
+import { Types } from "mongoose";
+import { BaseDocument } from "../common/base.types";
+import { IBaseUser } from "../people/user.types";
+
+export interface ISchool extends BaseDocument {
+  name: string;
+  address: string;
+  phoneNumber: string;
+  email: string;
+  website?: string;
+  establishedYear: number;
+  logoUrl: string;
+  isActive: boolean;
+  schoolCode: string;
+}
+
+export interface IClass extends BaseDocument {
+  school: Types.ObjectId | ISchool;
+  className: string;
+  stream: string;
+  academicYear: string;
+}
+
+export interface ISubject extends BaseDocument {
+  school: Types.ObjectId | ISchool;
+  subjectName: string;
+  classRef: Types.ObjectId | IClass;
+}
+
+export interface ITeacherSubject extends BaseDocument {
+  school: Types.ObjectId | ISchool;
+  teacherId: Types.ObjectId | IBaseUser;
+  subjectId: Types.ObjectId | ISubject;
+}
