41f1199b5c82591eae7c62227beae4c248b884e1
elainees-devs <elainees.devs@gmail.com>
2025-05-30 16:22:44 +0300

    feat(types): add IStudent interface and related types to student.ts

---
+++ server/src/types/people/student.ts
@@ -0,0 +1,24 @@
+//src/types/people/student.ts
+import { Types } from "mongoose";
+import { ISchool } from "../school/school";
+import { IBaseUser } from "./user";
+
+export type StudentGender = "boy" | "girl";
+export type StudentStatus = "active" | "inactive" | "completed" | "transferred";
+
+export interface IStudent {
+  school: string | ISchool;
+  first_name: string;
+  middle_name: string;
+  last_name: string;
+  student_gender: StudentGender;
+  date_of_birth: Date;
+  adm: number;
+  admission_date: Date;
+  previuosSchool?: string;
+  guardian: Types.ObjectId | IBaseUser;
+   status: StudentStatus;
+  studentId?: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
