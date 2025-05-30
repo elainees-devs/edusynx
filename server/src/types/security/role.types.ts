dea1e12846466d454723c0bdf905d7b77c76c479
elainees-devs <elainees.devs@gmail.com>
2025-05-30 19:10:20 +0300

    feat(types): add IRole interface for defining school roles with permissions and custom attributes

---
+++ server/src/types/security/role.types.ts
@@ -0,0 +1,17 @@
+//src/types/security/role.ts
+import { Types } from "mongoose";
+import { UserRole } from "../enum/enum";
+import { ISchool } from "../school/school.types";
+import { BaseDocument } from "../common/base.types";
+
+export interface IRole extends BaseDocument {
+  role: UserRole;
+  permissions: string[]; // Array of permission identifiers
+  createdBy: string; // User ID of the creator
+  updatedBy: string; // User ID of the last updater
+  isActive: boolean; // Indicates if the role is currently active
+  school: Types.ObjectId | ISchool;
+  isSystemRole?: boolean; // Indicates if this is a system-defined role
+  customAttributes?: Record<string, any>; // For any additional custom attributes
+  // e.g., { "color": "blue", "level": 1 }
+}
