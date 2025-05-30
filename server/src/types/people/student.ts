6bcb2cc8cb5e7d8a3c4e5e7ba50c9cd746f8ea1e
elainees-devs <elainees.devs@gmail.com>
2025-05-30 17:47:21 +0300

    refactor(student types): update IStudent interface to use camelCase and fix typo in previousSchool

--- server/src/types/people/student.ts
+++ server/src/types/people/student.ts
@@ -1,4 +1,4 @@
-//src/types/people/student.ts
+// src/types/people/student.ts
 import { Types } from "mongoose";
 import { IClass, ISchool } from "../school/school";
 import { IBaseUser } from "./user";
@@ -8,14 +8,14 @@ export type StudentStatus = "active" | "inactive" | "completed" | "transferred";
 
 export interface IStudent {
   school: string | ISchool;
-  first_name: string;
-  middle_name: string;
-  last_name: string;
-  student_gender: StudentGender;
-  date_of_birth: Date;
+  firstName: string;
+  middleName: string;
+  lastName: string;
+  studentGender: StudentGender;
+  dateOfBirth: Date;
   adm: number;
-  admission_date: Date;
-  previuosSchool?: string;
+  admissionDate: Date;
+  previousSchool?: string;
   guardian: Types.ObjectId | IBaseUser;
   class: Types.ObjectId | IClass;
   status: StudentStatus;
