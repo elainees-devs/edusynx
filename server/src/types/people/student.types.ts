d94a15f70b7b8607b7762bf8f36a62e86e5911f1
elainees-devs <elainees.devs@gmail.com>
2025-05-30 18:59:03 +0300

    refactor(types): extend IStudent from BaseDocument and update imports for clarity

---
+++ server/src/types/people/student.types.ts
@@ -0,0 +1,22 @@
+// src/types/people/student.ts
+import { Types } from "mongoose";
+import { IClass, ISchool } from "../school/school.types";
+import { IBaseUser } from "./user.types";
+import { StudentGender, StudentStatus } from "../enum/enum";
+import { BaseDocument } from "../common/base.types";
+
+export interface IStudent extends BaseDocument{
+  school: string | ISchool;
+  firstName: string;
+  middleName: string;
+  lastName: string;
+  studentGender: StudentGender;
+  dateOfBirth: Date;
+  adm: number;
+  admissionDate: Date;
+  previousSchool?: string;
+  guardian: Types.ObjectId | IBaseUser;
+  class: Types.ObjectId | IClass;
+  status: StudentStatus;
+  studentId?: string;
+}
