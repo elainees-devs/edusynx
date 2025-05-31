31a5605fff03deadfd2ab187054e93a9073afff7
elainees-devs <elainees.devs@gmail.com>
2025-05-31 18:48:20 +0300

    feat(models): add Notification model with support for title, message, recipient, and read status

---
+++ server/src/models/notification.model.ts
@@ -0,0 +1,19 @@
+//src/models/notification.model.ts
+import { Schema, model, Types } from 'mongoose';
+import { INotification } from '../types/school/school-activity.types'; // Adjust import path as needed
+
+const notificationSchema = new Schema<INotification>(
+  {
+    school: { type: Schema.Types.ObjectId, ref: 'School', required: true },
+    notificationTitle: { type: String, required: true },
+    message: { type: String, required: true },
+    recipient: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    isRead: { type: Boolean, required: true, default: false },
+    readAt: { type: Date },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Notification = model<INotification>('Notification', notificationSchema);
