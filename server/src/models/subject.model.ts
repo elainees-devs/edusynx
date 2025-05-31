bed9de4a71eb4da3507429a004a3827a6d69e901
elainees-devs <elainees.devs@gmail.com>
2025-05-31 18:33:10 +0300

    feat: create Mongoose model for ISubject interface

---
+++ server/src/models/subject.model.ts
@@ -0,0 +1,16 @@
+//src/models/subject.model.ts
+import { Schema, Types, model } from "mongoose";
+import { ISubject } from "../types/school/school-core.types";
+
+const SubjectSchema = new Schema<ISubject>(
+  {
+    school: { type: Types.ObjectId, ref: 'School', required: true },
+    subjectName: { type: String, required: true },
+    classRef: { type: Types.ObjectId, ref: 'Class', required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const SubjectModel = model<ISubject>('Subject', SubjectSchema);
