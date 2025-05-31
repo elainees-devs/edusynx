473b2d43de729b5c0c1340e1133fab3721e35354
elainees-devs <elainees.devs@gmail.com>
2025-05-31 18:28:34 +0300

    feat: add class model for IClass interface

---
+++ server/src/models/class.model.ts
@@ -0,0 +1,17 @@
+//src/models/class.model.ts
+import { Schema, model, Document, Types } from "mongoose";
+import { IClass } from "../types/school/school-core.types";
+
+const ClassSchema = new Schema<IClass>(
+  {
+    school: { type: Types.ObjectId, ref: "School", required: true },
+    className: { type: String, required: true },
+    stream: { type: String, required: true },
+    academicYear: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const ClassModel = model<IClass>("Class", ClassSchema);
