d02d50a8609f53029194e579f9d350c7fc34f287
elainees-devs <elainees.devs@gmail.com>
2025-06-02 19:12:57 +0300

    feat: add shared Create DTOs for user, school, and exam in entity.dto.ts

---
+++ server/src/dto/entity.dto.ts
@@ -0,0 +1,20 @@
+//src/dto/entity.dto.ts
+import { IBaseUser, IExam, ISchool } from "../types";
+
+// User DTO
+export type CreateUserDTO = Omit<
+  IBaseUser,
+  "id" | "_id" | "createdAt" | "updatedAt"
+>;
+
+// School DTO
+export type CreateSchoolDTO = Omit<
+  ISchool,
+  "id" | "_id" | "createdAt" | "updatedAt"
+>;
+
+// Exam DTO
+export type CreateExamDTO = Omit<
+  IExam,
+  "id" | "_id" | "createdAt" | "updatedAt"
+>;
