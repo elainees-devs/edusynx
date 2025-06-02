7876c1abe7c4fed7ffb86af3a581e5a507b8f86f
elainees-devs <elainees.devs@gmail.com>
2025-06-02 19:25:52 +0300

    feat: add CreateExamResultDTO to entity.dto.ts for exam result creation

--- server/src/dto/entity.dto.ts
+++ server/src/dto/entity.dto.ts
@@ -43,3 +43,9 @@ export type CreateEventDTO = Omit<
   IEvent,
   "id" | "_id" | "createdAt" | "updatedAt"
 >;
+
+//Exam Result DTO
+export type CreateExamResultDTO = Omit<
+  IExam,
+  "id" | "_id" | "createdAt" | "updatedAt"
+>;
