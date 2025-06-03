59508fc92d5711c43846b1feb17d8c6561d7a948
elainees-devs <elainees.devs@gmail.com>
2025-06-03 17:16:23 +0300

    refactor(types): rename schoolId to school and support ISchool | ObjectId in IInvoiceCounter

--- server/src/types/finance/counter.types.ts
+++ server/src/types/finance/counter.types.ts
@@ -1,8 +1,10 @@
 //src/types/finance/counter.types.ts
 import { Types } from "mongoose";
 import { BaseDocument } from "../common/base.types";
+import { ISchool } from "../school/school-core.types";
+
 
 export interface IInvoiceCounter extends BaseDocument {
-  schoolId: Types.ObjectId;
+  school: Types.ObjectId | ISchool;
   seq: number;
 }
