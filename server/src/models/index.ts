37742e6c75e86ba90fdb907118109b8cfbfbcf70
elainees-devs <elainees.devs@gmail.com>
2025-06-03 15:48:46 +0300

    feat(models): export all model modules from index for centralized access

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -1,2 +1,14 @@
-//src/models/school.index.ts
+//src/models/index.ts
 export * from './school.model';
+export * from './user.model';
+export * from './guardian.model';
+export * from './teacher.model';
+export * from './student.model';
+export * from './class.model';          
+export * from './attendance.model';
+export * from './exam.model';
+export * from './fee.model';
+export * from './invoice.model';
+export * from './financialReport.model';
+export * from './event.model';
+export * from './feePayment.model';
