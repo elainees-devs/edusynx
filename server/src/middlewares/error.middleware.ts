10b2e7c72cdfa74ad289eb820414e44ee7518c53
elainees-devs <elainees.devs@gmail.com>
2025-06-03 15:34:08 +0300

    feat: add centralized error handling middleware

---
+++ server/src/middlewares/error.middleware.ts
@@ -0,0 +1,17 @@
+//src/middlewares/error.middleware.ts
+import { Request, Response, NextFunction } from "express";
+import logger from "../utils/logger";
+
+export function errorHandler(
+  err: any,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  logger.error(`${req.method} ${req.url} - ${err.message}\n${err.stack}`);
+
+  res.status(err.status || 500).json({
+    success: false,
+    message: err.message || "Internal Server Error",
+  });
+}
