ae24f0dac37b846b3a364c185839b8e02ce2a704
elainees-devs <elainees.devs@gmail.com>
2025-06-03 20:27:48 +0300

    Refactor user.repository to use unified UserModel import from models index

--- server/src/repositories/user.repository.ts
+++ server/src/repositories/user.repository.ts
@@ -1,11 +1,11 @@
 // src/repositories/user.repository.ts
 import { CreateUserDTO } from "../dto/entity.dto";
-import { User } from "../models/user.model";
+import { UserModel } from "../models";
 import { IBaseUser } from "../types/people/user.types";
 
 export class UserRepository {
   async createUser(userData: CreateUserDTO): Promise<IBaseUser> {
-    const user = new User(userData);
+    const user = new UserModel(userData);
     return await user.save();
   }
 
@@ -13,22 +13,22 @@ export class UserRepository {
     id: string,
     updates: Partial<IBaseUser>
   ): Promise<IBaseUser | null> {
-    return await User.findByIdAndUpdate(id, updates, { new: true }).exec();
+    return await UserModel.findByIdAndUpdate(id, updates, { new: true }).exec();
   }
 
   async findUserById(id: string): Promise<IBaseUser | null> {
-    return await User.findById(id).populate("school", "name").exec();
+    return await UserModel.findById(id).populate("school", "name").exec();
   }
 
   async findAllUsers(): Promise<IBaseUser[]> {
-    return await User.find().populate("school", "name").exec();
+    return await UserModel.find().populate("school", "name").exec();
   }
 
   async deleteUserById(id: string): Promise<IBaseUser | null> {
-    return await User.findByIdAndDelete(id).exec();
+    return await UserModel.findByIdAndDelete(id).exec();
   }
 
   async deleteAllUsers(): Promise<void> {
-    await User.deleteMany({}).exec();
+    await UserModel.deleteMany({}).exec();
   }
 }
