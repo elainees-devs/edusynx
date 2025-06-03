bb7859609fbf52713d8e9d0af3609648aa0def41
elainees-devs <elainees.devs@gmail.com>
2025-06-03 10:07:39 +0300

    refactor(repository): rename UserRepository methods for clarity and consistency

--- server/src/repository/user.repository.ts
+++ server/src/repository/user.repository.ts
@@ -4,31 +4,31 @@ import { User } from "../models/user.model";
 import { IBaseUser } from "../types/people/user.types";
 
 export class UserRepository {
-  async create(userData: CreateUserDTO): Promise<IBaseUser> {
+  async createUser(userData: CreateUserDTO): Promise<IBaseUser> {
     const user = new User(userData);
     return await user.save();
   }
 
-  async updateById(
+  async updateUserById(
     id: string,
     updates: Partial<IBaseUser>
   ): Promise<IBaseUser | null> {
     return await User.findByIdAndUpdate(id, updates, { new: true }).exec();
   }
 
-  async findById(id: string): Promise<IBaseUser | null> {
+  async findUserById(id: string): Promise<IBaseUser | null> {
     return await User.findById(id).populate("school", "name").exec();
   }
 
-  async findAll(): Promise<IBaseUser[]> {
+  async findAllUsers(): Promise<IBaseUser[]> {
     return await User.find().populate("school", "name").exec();
   }
 
-  async deleteById(id: string): Promise<IBaseUser | null> {
+  async deleteUserById(id: string): Promise<IBaseUser | null> {
     return await User.findByIdAndDelete(id).exec();
   }
 
-  async deleteAll(): Promise<void> {
+  async deleteAllUsers(): Promise<void> {
     await User.deleteMany({}).exec();
   }
 }
