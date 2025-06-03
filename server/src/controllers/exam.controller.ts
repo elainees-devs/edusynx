38e0fe2c440207953ef8a41b7d73eef63a08c403
elainees-devs <elainees.devs@gmail.com>
2025-06-03 21:46:00 +0300

    feat: add exam controller with CRUD operations and full population of related fields

---
+++ server/src/controllers/exam.controller.ts
@@ -0,0 +1,64 @@
+//src/controllers/exam.controller.ts
+// src/controllers/exam.controller.ts
+import { Request, Response, NextFunction } from "express";
+import { ExamRepository } from "../repositories/exam.repository";
+import { AppError } from "../utils/AppError";
+
+const examRepo = new ExamRepository();
+
+export const createExam = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const newExam = await examRepo.createExam(req.body);
+    res.status(201).json(newExam);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getExamById = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const exam = await examRepo.findExamById(req.params.id);
+    if (!exam) throw new AppError("Exam not found", 404);
+    res.json(exam);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getAllExams = async (_req: Request, res: Response, next: NextFunction) => {
+  try {
+    const exams = await examRepo.findAllExams();
+    res.json(exams);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateExam = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const updatedExam = await examRepo.updateExamById(req.params.id, req.body);
+    if (!updatedExam) throw new AppError("Exam not found", 404);
+    res.json(updatedExam);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteExam = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const deletedExam = await examRepo.deleteExamById(req.params.id);
+    if (!deletedExam) throw new AppError("Exam not found", 404);
+    res.status(204).send();
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteAllExams = async (_req: Request, res: Response, next: NextFunction) => {
+  try {
+    await examRepo.deleteAllExams();
+    res.status(204).send();
+  } catch (error) {
+    next(error);
+  }
+};
