f098e7e8f99622004c42b060259cc6f20c179907
elainees-devs <elainees.devs@gmail.com>
2025-06-04 10:30:35 +0300

    feat(controller): export all controllers from central index file

---
+++ server/src/controllers/index.ts
@@ -0,0 +1,19 @@
+//src/controllers/index.ts
+
+export * from "./session.controller";
+export * from "./login.controller";
+export * from "./user.controller";
+export * from "./notification.controller";   
+export * from "./school.controller";  
+export * from "./class.controller"; 
+export * from "./student.controller";
+export * from "./attendance.controller";
+export * from "./exam.controller";
+export * from "./subject.controller";
+export * from "./invoice.controller";
+export * from "./invoiceItem.controller";
+export * from "./fee.controller";
+export * from "./feePayment.controller";
+export * from "./payment.controller";
+export * from "./permission.controller";
+export * from "./rolePermission.controller";
