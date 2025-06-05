945d3ef57c752ec0ac83ef57311168fad70526e7
elainees-devs <elainees.devs@gmail.com>
2025-06-05 15:57:35 +0300

    refactor(controller): convert user controller functions to class-based structure
    - Replaced standalone controller functions with a class-based UserController
    - Encapsulated repository access in the class
    - Prepared controller for improved scalability and dependency injection support

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -1,55 +1,60 @@
-//src/controllers/users.controller.ts
+// src/controllers/users.controller.ts
 import { Request, Response, NextFunction } from "express";
 import { UserRepository } from "../repositories/user.repository";
 import { AppError } from "../utils/AppError";
 
+export class UserController {
+  private userRepo: UserRepository;
 
-const userRepo = new UserRepository();
-
-export const createUser = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const newUser = await userRepo.createUser(req.body);
-    res.status(201).json(newUser);
-  } catch (error) {
-    next(error);
+  constructor() {
+    this.userRepo = new UserRepository();
   }
-};
 
-export const getAllUsers = async (_req: Request, res: Response, next: NextFunction) => {
-  try {
-    const users = await userRepo.findAllUsers();
-    res.json(users);
-  } catch (error) {
-    next(error);
-  }
-};
+  createUser = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const newUser = await this.userRepo.createUser(req.body);
+      res.status(201).json(newUser);
+    } catch (error) {
+      next(error);
+    }
+  };
 
-export const getUserById = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const user = await userRepo.findUserById(req.params.id);
-    if (!user) throw new AppError("User not found", 404);
-    res.json(user);
-  } catch (error) {
-    next(error);
-  }
-};
+  getAllUsers = async (_req: Request, res: Response, next: NextFunction) => {
+    try {
+      const users = await this.userRepo.findAllUsers();
+      res.json(users);
+    } catch (error) {
+      next(error);
+    }
+  };
 
-export const updateUser = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const updatedUser = await userRepo.updateUserById(req.params.id, req.body);
-    if (!updatedUser) throw new AppError("User not found", 404);
-    res.json(updatedUser);
-  } catch (error) {
-    next(error);
-  }
-};
+  getUserById = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const user = await this.userRepo.findUserById(req.params.id);
+      if (!user) throw new AppError("User not found", 404);
+      res.json(user);
+    } catch (error) {
+      next(error);
+    }
+  };
 
-export const deleteUser = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const deletedUser = await userRepo.deleteUserById(req.params.id);
-    if (!deletedUser) throw new AppError("User not found", 404);
-    res.status(204).send();
-  } catch (error) {
-    next(error);
-  }
-};
+  updateUser = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const updatedUser = await this.userRepo.updateUserById(req.params.id, req.body);
+      if (!updatedUser) throw new AppError("User not found", 404);
+      res.json(updatedUser);
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  deleteUser = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const deletedUser = await this.userRepo.deleteUserById(req.params.id);
+      if (!deletedUser) throw new AppError("User not found", 404);
+      res.status(204).send();
+    } catch (error) {
+      next(error);
+    }
+  };
+}
