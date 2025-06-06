39120b5d77483f8106564c56b6de36507a9fcb8d
elainees-devs <elainees.devs@gmail.com>
2025-06-06 18:49:42 +0300

    feat: add school routes with validation and controller integration

---
+++ server/src/routes/school.route.ts
@@ -0,0 +1,26 @@
+//src/routes/school.route.ts
+import { Router } from "express";
+import { validate } from "../middlewares/validate";
+import {
+  createSchoolSchema,
+  updateSchoolSchema,
+} from "../validation/school.schema";
+import { SchoolController } from "../controllers";
+
+const schoolRouter = Router();
+const schoolController = new SchoolController();
+
+schoolRouter.post(
+  "/",
+  validate(createSchoolSchema),
+  schoolController.createSchool
+);
+schoolRouter.get("/:id", schoolController.getSchoolById);
+schoolRouter.get("/", schoolController.getAllSchools);
+schoolRouter.put(
+  "/:id",
+  validate(updateSchoolSchema),
+  schoolController.updateSchool
+);
+schoolRouter.delete("/:id", schoolController.deleteSchoolById);
+schoolRouter.delete("/", schoolController.deleteAllSchools);
