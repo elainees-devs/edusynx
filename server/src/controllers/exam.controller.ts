ea58938dd0d5fdf7b9248a4be618af5eeebff747
elainees-devs <elainees.devs@gmail.com>
2025-06-06 17:23:58 +0300

    refactor: apply handleAsync to exam controller for cleaner error handling

--- server/src/controllers/exam.controller.ts
+++ server/src/controllers/exam.controller.ts
@@ -1,63 +1,40 @@
 //src/controllers/exam.controller.ts
-import { Request, Response, NextFunction } from "express";
+import { Request, Response } from "express";
 import { ExamRepository } from "../repositories/exam.repository";
 import { AppError } from "../utils/AppError";
+import { handleAsync } from "../utils/handleAsync";
 
 const examRepo = new ExamRepository();
 
-export const createExam = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const newExam = await examRepo.createExam(req.body);
-    res.status(201).json(newExam);
-  } catch (error) {
-    next(error);
-  }
-};
+export const createExam = handleAsync(async (req, res) => {
+  const newExam = await examRepo.createExam(req.body);
+  res.status(201).json(newExam);
+});
 
-export const getExamById = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const exam = await examRepo.findExamById(req.params.id);
-    if (!exam) throw new AppError("Exam not found", 404);
-    res.json(exam);
-  } catch (error) {
-    next(error);
-  }
-};
+export const getExamById = handleAsync<{ id: string }>(async (req, res) => {
+  const exam = await examRepo.findExamById(req.params.id);
+  if (!exam) throw new AppError("Exam not found", 404);
+  res.json(exam);
+});
 
-export const getAllExams = async (_req: Request, res: Response, next: NextFunction) => {
-  try {
-    const exams = await examRepo.findAllExams();
-    res.json(exams);
-  } catch (error) {
-    next(error);
-  }
-};
+export const getAllExams = handleAsync(async (_req, res) => {
+  const exams = await examRepo.findAllExams();
+  res.json(exams);
+});
 
-export const updateExam = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const updatedExam = await examRepo.updateExamById(req.params.id, req.body);
-    if (!updatedExam) throw new AppError("Exam not found", 404);
-    res.json(updatedExam);
-  } catch (error) {
-    next(error);
-  }
-};
+export const updateExam = handleAsync<{ id: string }, any, Partial<any>>(async (req, res) => {
+  const updatedExam = await examRepo.updateExamById(req.params.id, req.body);
+  if (!updatedExam) throw new AppError("Exam not found", 404);
+  res.json(updatedExam);
+});
 
-export const deleteExam = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const deletedExam = await examRepo.deleteExamById(req.params.id);
-    if (!deletedExam) throw new AppError("Exam not found", 404);
-    res.status(204).send();
-  } catch (error) {
-    next(error);
-  }
-};
+export const deleteExam = handleAsync<{ id: string }>(async (req, res) => {
+  const deletedExam = await examRepo.deleteExamById(req.params.id);
+  if (!deletedExam) throw new AppError("Exam not found", 404);
+  res.status(204).send();
+});
 
-export const deleteAllExams = async (_req: Request, res: Response, next: NextFunction) => {
-  try {
-    await examRepo.deleteAllExams();
-    res.status(204).send();
-  } catch (error) {
-    next(error);
-  }
-};
+export const deleteAllExams = handleAsync(async (_req, res) => {
+  await examRepo.deleteAllExams();
+  res.status(204).send();
+});
