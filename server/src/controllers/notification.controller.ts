7da55c85c72a2d7968b1983bbaf56c6422fcc5a4
elainees-devs <elainees.devs@gmail.com>
2025-06-06 17:44:16 +0300

    Refactor notification controller to use instance methods with handleAsync wrapper and improved async error handling

--- server/src/controllers/notification.controller.ts
+++ server/src/controllers/notification.controller.ts
@@ -1,42 +1,28 @@
-import { Request, Response, NextFunction } from "express";
+//src/controllers/notification.controller.ts
+import { Request, Response } from "express";
 import { NotificationRepository } from "../repositories/notification.repository";
 import { AppError } from "../utils/AppError";
+import { handleAsync } from "../utils/handleAsync";
 
 const notificationRepo = new NotificationRepository();
 
-export const createNotification = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const notification = await NotificationRepository.create(req.body);
-    res.status(201).json(notification);
-  } catch (error) {
-    next(error);
-  }
-};
+export const createNotification = handleAsync(async (req: Request, res: Response) => {
+  const notification = await notificationRepo.create(req.body);
+  res.status(201).json(notification);
+});
 
-export const markNotificationAsRead = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const updatedNotification = await NotificationRepository.markAsRead(req.params.id);
-    if (!updatedNotification) throw new AppError("Notification not found", 404);
-    res.json(updatedNotification);
-  } catch (error) {
-    next(error);
-  }
-};
+export const markNotificationAsRead = handleAsync(async (req: Request, res: Response) => {
+  const updatedNotification = await notificationRepo.markAsRead(req.params.id);
+  if (!updatedNotification) throw new AppError("Notification not found", 404);
+  res.json(updatedNotification);
+});
 
-export const getUserNotifications = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const notifications = await NotificationRepository.getUserNotifications(req.params.userId);
-    res.json(notifications);
-  } catch (error) {
-    next(error);
-  }
-};
+export const getUserNotifications = handleAsync(async (req: Request, res: Response) => {
+  const notifications = await notificationRepo.getUserNotifications(req.params.userId);
+  res.json(notifications);
+});
 
-export const deleteNotification = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    await NotificationRepository.delete(req.params.id);
-    res.status(204).send();
-  } catch (error) {
-    next(error);
-  }
-};
+export const deleteNotification = handleAsync(async (req: Request, res: Response) => {
+  const deleteNotification = notificationRepo.deleteNotification(req.params.id);
+  res.status(204).send();
+});
