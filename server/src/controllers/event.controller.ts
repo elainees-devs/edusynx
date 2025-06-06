ed1f8888c8d7f0e377b5314c503e1cb9cb8fedd4
elainees-devs <elainees.devs@gmail.com>
2025-06-06 17:22:29 +0300

    refactor: simplify event controller using handleAsync utility

--- server/src/controllers/event.controller.ts
+++ server/src/controllers/event.controller.ts
@@ -1,71 +1,40 @@
 //src/controllers/event.controller.ts
-import { Request, Response, NextFunction } from "express";
 import { EventRepository } from "../repositories/event.repository";
 import { AppError } from "../utils/AppError";
+import { handleAsync } from "../utils/handleAsync";
 
-export const createEvent = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const eventData = req.body;
-    const eventRepository = new EventRepository();
-    const newEvent = await eventRepository.createEvent(eventData);
-    res.status(201).json(newEvent);
-  } catch (error) {
-    next(error);
-  }
-}
+const eventRepository = new EventRepository();
 
-export const getEventById = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const eventId = req.params.id;
-    const eventRepository = new EventRepository();
-    const event = await eventRepository.getEventById(eventId);
-    if (!event) throw new AppError("Event not found", 404);
-    res.json(event);
-  } catch (error) {
-    next(error);
-  }
-}   
+export const createEvent = handleAsync(async (req, res) => {
+  const eventData = req.body;
+  const newEvent = await eventRepository.createEvent(eventData);
+  res.status(201).json(newEvent);
+});
 
-export const getAllEvents = async (_req: Request, res: Response, next: NextFunction) => {
-  try {
-    const eventRepository = new EventRepository();
-    const events = await eventRepository.getAllEvents();
-    res.json(events);
-  } catch (error) {
-    next(error);
-  }
-}       
+export const getEventById = handleAsync<{ id: string }>(async (req, res) => {
+  const event = await eventRepository.getEventById(req.params.id);
+  if (!event) throw new AppError("Event not found", 404);
+  res.json(event);
+});
 
-export const updateEvent = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const eventId = req.params.id;
-    const eventRepository = new EventRepository();
-    const updatedEvent = await eventRepository.updateEventById(eventId, req.body);
-    if (!updatedEvent) throw new AppError("Event not found", 404);
-    res.json(updatedEvent);
-  } catch (error) {
-    next(error);
-  }
-}   
+export const getAllEvents = handleAsync(async (_req, res) => {
+  const events = await eventRepository.getAllEvents();
+  res.json(events);
+});
 
-export const deleteEvent = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const eventId = req.params.id;
-    const eventRepository = new EventRepository();
-    const deletedEvent = await eventRepository.deleteEventById(eventId);
-    if (!deletedEvent) throw new AppError("Event not found", 404);
-    res.status(204).send();
-  } catch (error) {
-    next(error);
-  }
-}   
+export const updateEvent = handleAsync<{ id: string }, any, Partial<any>>(async (req, res) => {
+  const updatedEvent = await eventRepository.updateEventById(req.params.id, req.body);
+  if (!updatedEvent) throw new AppError("Event not found", 404);
+  res.json(updatedEvent);
+});
 
-export const deleteAllEvents = async (_req: Request, res: Response, next: NextFunction) => {
-  try {
-    const eventRepository = new EventRepository();
-    await eventRepository.deleteAllEvents();
-    res.status(204).send();
-  } catch (error) {
-    next(error);
-  }
-}
+export const deleteEvent = handleAsync<{ id: string }>(async (req, res) => {
+  const deletedEvent = await eventRepository.deleteEventById(req.params.id);
+  if (!deletedEvent) throw new AppError("Event not found", 404);
+  res.status(204).send();
+});
+
+export const deleteAllEvents = handleAsync(async (_req, res) => {
+  await eventRepository.deleteAllEvents();
+  res.status(204).send();
+});
