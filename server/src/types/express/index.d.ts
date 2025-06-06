3fe700a6ff146a92fe8726653eb6e184a7c77c8a
elainees-devs <elainees.devs@gmail.com>
2025-06-06 17:04:40 +0300

    refactor(types): update Express Request user type to use base interfaces

--- server/src/types/express/index.d.ts
+++ server/src/types/express/index.d.ts
@@ -1,10 +1,11 @@
 //src/types/express/index.d.ts
-import { IGuardianUser, ITeacherUser } from "../people/user.types";
+import {IGuardian, ITeacher} from "../people/user.types";
 
 declare global {
   namespace Express {
     interface Request {
-      user?: IGuardianUser | ITeacherUser;
+      user?: IGuardian | ITeacher;
     }
   }
 }
+
