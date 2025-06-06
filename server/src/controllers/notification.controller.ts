bda53efdebfcfbfef213c465a20b3bf8e6875659
elainees-devs <elainees.devs@gmail.com>
2025-06-06 19:07:22 +0300

    Refactor notification.controller.ts to use handleAsync and fix missing await in deleteNotification

--- server/src/controllers/notification.controller.ts
+++ server/src/controllers/notification.controller.ts
@@ -1,28 +1,28 @@
 //src/controllers/notification.controller.ts
-import { Request, Response } from "express";
 import { NotificationRepository } from "../repositories/notification.repository";
 import { AppError } from "../utils/AppError";
 import { handleAsync } from "../utils/handleAsync";
 
 const notificationRepo = new NotificationRepository();
 
-export const createNotification = handleAsync(async (req: Request, res: Response) => {
+export const createNotification = handleAsync(async (req, res) => {
   const notification = await notificationRepo.create(req.body);
   res.status(201).json(notification);
 });
 
-export const markNotificationAsRead = handleAsync(async (req: Request, res: Response) => {
+export const markNotificationAsRead = handleAsync(async (req, res) => {
   const updatedNotification = await notificationRepo.markAsRead(req.params.id);
   if (!updatedNotification) throw new AppError("Notification not found", 404);
   res.json(updatedNotification);
 });
 
-export const getUserNotifications = handleAsync(async (req: Request, res: Response) => {
+export const getUserNotifications = handleAsync(async (req, res) => {
   const notifications = await notificationRepo.getUserNotifications(req.params.userId);
   res.json(notifications);
 });
 
-export const deleteNotification = handleAsync(async (req: Request, res: Response) => {
-  const deleteNotification = notificationRepo.deleteNotification(req.params.id);
+export const deleteNotification = handleAsync(async (req, res) => {
+  await notificationRepo.deleteNotification(req.params.id);
   res.status(204).send();
 });
+
