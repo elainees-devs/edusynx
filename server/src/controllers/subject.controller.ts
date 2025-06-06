bd1b271d00c01af69edfd28e056e1a5f72f7d50f
elainees-devs <elainees.devs@gmail.com>
2025-06-06 19:16:26 +0300

    Refactor subject.controller.ts to use handleAsync and simplify typings

--- server/src/controllers/subject.controller.ts
+++ server/src/controllers/subject.controller.ts
@@ -1,34 +1,33 @@
 // src/controllers/subject.controller.ts
-import { Request, Response } from "express";
 import { SubjectRepository } from "../repositories/subject.repository";
 import { AppError } from "../utils/AppError";
 import { handleAsync } from "../utils/handleAsync";
 
 const subjectRepo = new SubjectRepository();
 
-export const createSubject = handleAsync(async (req: Request, res: Response) => {
+export const createSubject = handleAsync(async (req, res) => {
   const subject = await subjectRepo.createSubject(req.body);
   res.status(201).json(subject);
 });
 
-export const getSubjectsBySchool = handleAsync(async (req: Request, res: Response) => {
+export const getSubjectsBySchool = handleAsync(async (req, res) => {
   const subjects = await subjectRepo.getSubjectsBySchool(req.params.schoolId);
   res.json(subjects);
 });
 
-export const getSubjectById = handleAsync(async (req: Request, res: Response) => {
+export const getSubjectById = handleAsync(async (req, res) => {
   const subject = await subjectRepo.getSubjectById(req.params.id);
   if (!subject) throw new AppError("Subject not found", 404);
   res.json(subject);
 });
 
-export const updateSubject = handleAsync(async (req: Request, res: Response) => {
+export const updateSubject = handleAsync(async (req, res) => {
   const updatedSubject = await subjectRepo.updateSubject(req.params.id, req.body);
   if (!updatedSubject) throw new AppError("Subject not found", 404);
   res.json(updatedSubject);
 });
 
-export const deleteSubject = handleAsync(async (req: Request, res: Response) => {
+export const deleteSubject = handleAsync(async (req, res) => {
   const deletedSubject = await subjectRepo.deleteSubject(req.params.id);
   if (!deletedSubject) throw new AppError("Subject not found", 404);
   res.status(204).send();
