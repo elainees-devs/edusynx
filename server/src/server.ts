9aa6013a3828bd0aa565315f33f9d67960e13344
elainees-devs <elainees.devs@gmail.com>
2025-06-06 16:22:09 +0300

    refactor: add user routes and integrate modular routing in server setup

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,14 @@
 // src/server.ts
-import express, { Request, Response } from 'express';
-import { configDotenv } from 'dotenv';
-import connectDB from './config/db';
-import logger from './utils/logger';  // Import logger from utils folder
+import express, { Request, Response } from "express";
+import { configDotenv } from "dotenv";
+import connectDB from "./config/db";
+import logger from "./utils/logger";
+import router from "./routes/user.route";
 
 configDotenv();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT: number = parseInt(process.env.PORT || "5000", 10);
 
 // Database connection
 connectDB();
@@ -16,9 +17,10 @@ connectDB();
 app.use(express.json());
 
 // Routes
-app.get('/', (req: Request, res: Response) => {
-  logger.info('Received request on /'); 
-  res.send('Server is running....!');
+app.use("/users", router);
+app.get("/", (req: Request, res: Response) => {
+  logger.info("Received request on /");
+  res.send("Server is running....!");
 });
 
 app.listen(PORT, () => {
