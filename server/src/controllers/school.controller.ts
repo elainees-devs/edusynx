17d5b17023eb717597eeb548bacf44cb642a62fd
elainees-devs <elainees.devs@gmail.com>
2025-06-06 18:08:07 +0300

    refactor(school.controller): unify async error handling using handleAsync wrapper and instance methods

--- server/src/controllers/school.controller.ts
+++ server/src/controllers/school.controller.ts
@@ -1,54 +1,35 @@
 // src/controllers/school.controller.ts
-import { Request, Response, NextFunction } from "express";
+import { Request, Response } from "express";
 import { SchoolRepository } from "../repositories/school.repository";
 import { AppError } from "../utils/AppError";
+import { handleAsync } from "../utils/handleAsync";
 
 const schoolRepo = new SchoolRepository();
 
-export const createSchool = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const newSchool = await schoolRepo.createSchool(req.body);
-    res.status(201).json(newSchool);
-  } catch (error) {
-    next(error);
-  }
-};
+export const createSchool = handleAsync(async (req: Request, res: Response) => {
+  const newSchool = await schoolRepo.createSchool(req.body);
+  res.status(201).json(newSchool);
+});
 
-export const getAllSchools = async (_req: Request, res: Response, next: NextFunction) => {
-  try {
-    const schools = await schoolRepo.findAllSchools();
-    res.json(schools);
-  } catch (error) {
-    next(error);
-  }
-};
+export const getAllSchools = handleAsync(async (_req: Request, res: Response) => {
+  const schools = await schoolRepo.findAllSchools();
+  res.json(schools);
+});
 
-export const getSchoolById = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const school = await schoolRepo.findSchoolById(req.params.id);
-    if (!school) throw new AppError("School not found", 404);
-    res.json(school);
-  } catch (error) {
-    next(error);
-  }
-};
+export const getSchoolById = handleAsync(async (req: Request, res: Response) => {
+  const school = await schoolRepo.findSchoolById(req.params.id);
+  if (!school) throw new AppError("School not found", 404);
+  res.json(school);
+});
 
-export const updateSchool = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const updated = await schoolRepo.updateSchoolById(req.params.id, req.body);
-    if (!updated) throw new AppError("School not found", 404);
-    res.json(updated);
-  } catch (error) {
-    next(error);
-  }
-};
+export const updateSchool = handleAsync(async (req: Request, res: Response) => {
+  const updated = await schoolRepo.updateSchoolById(req.params.id, req.body);
+  if (!updated) throw new AppError("School not found", 404);
+  res.json(updated);
+});
 
-export const deleteSchool = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const deleted = await schoolRepo.deleteSchoolById(req.params.id);
-    if (!deleted) throw new AppError("School not found", 404);
-    res.status(204).send();
-  } catch (error) {
-    next(error);
-  }
-};
+export const deleteSchool = handleAsync(async (req: Request, res: Response) => {
+  const deleted = await schoolRepo.deleteSchoolById(req.params.id);
+  if (!deleted) throw new AppError("School not found", 404);
+  res.status(204).send();
+});
