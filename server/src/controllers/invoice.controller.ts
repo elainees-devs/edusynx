d31902275bb0fb38ba4c272703644cac7c5e4525
elainees-devs <elainees.devs@gmail.com>
2025-06-06 19:00:09 +0300

    Refactor invoice.controller.ts to use generic handleAsync types and simplify Request/Response typings

--- server/src/controllers/invoice.controller.ts
+++ server/src/controllers/invoice.controller.ts
@@ -1,40 +1,40 @@
 // src/controllers/invoice.controller.ts
-import { Request, Response } from "express";
 import { InvoiceRepository } from "../repositories/invoice.repository";
 import { AppError } from "../utils/AppError";
 import { handleAsync } from "../utils/handleAsync";
 
 const invoiceRepo = new InvoiceRepository();
 
-export const createInvoice = handleAsync(async (req: Request, res: Response) => {
+export const createInvoice = handleAsync(async (req, res) => {
   const newInvoice = await invoiceRepo.createInvoice(req.body);
   res.status(201).json(newInvoice);
 });
 
-export const getInvoiceById = handleAsync(async (req: Request, res: Response) => {
+export const getInvoiceById = handleAsync<{ id: string }>(async (req, res) => {
   const invoice = await invoiceRepo.getInvoiceById(req.params.id);
   if (!invoice) throw new AppError("Invoice not found", 404);
   res.json(invoice);
 });
 
-export const updateInvoice = handleAsync(async (req: Request, res: Response) => {
+export const updateInvoice = handleAsync(async (req: any, res: any) => {
   const updated = await invoiceRepo.updateInvoice(req.params.id, req.body);
   if (!updated) throw new AppError("Invoice not found", 404);
   res.json(updated);
 });
 
-export const deleteInvoice = handleAsync(async (req: Request, res: Response) => {
+
+export const deleteInvoice = handleAsync<{ id: string }>(async (req, res) => {
   const deleted = await invoiceRepo.deleteInvoice(req.params.id);
   if (!deleted) throw new AppError("Invoice not found", 404);
   res.status(204).send();
 });
 
-export const getAllInvoices = handleAsync(async (req: Request, res: Response) => {
+export const getAllInvoices = handleAsync(async (req, res) => {
   const invoices = await invoiceRepo.getAllInvoices(req.query);
   res.json(invoices);
 });
 
-export const deleteAllInvoices = handleAsync(async (_req: Request, res: Response) => {
+export const deleteAllInvoices = handleAsync(async (_req, res) => {
   await invoiceRepo.deleteAllInvoices();
   res.status(204).send();
 });
