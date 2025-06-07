f1ec16630088b0f4a4c6d543f2b9d3cc30b8d0dd
elainees-devs <elainees.devs@gmail.com>
2025-06-07 15:17:04 +0300

    refactor(models): update LoginHistory model to use ILogin interface for full login info

--- server/src/models/loginHistory.model.ts
+++ server/src/models/loginHistory.model.ts
@@ -1,12 +1,17 @@
 // src/models/loginHistory.model.ts
 import { Schema, model, Types } from "mongoose";
-import { ILoginBase } from "../types/common/auth-context.types";
+import { ILogin } from "../types/security/login.types";
 
-const LoginHistorySchema = new Schema<ILoginBase & { userId: Types.ObjectId }>({
+const LoginHistorySchema = new Schema<ILogin>({
   userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  sessionId: { type: Schema.Types.ObjectId, ref: "Session" },
+  isSuccessful: { type: Boolean, required: true },
+  failureReason: { type: String },
+
+  // from ILoginBase
   loginTime: { type: Date, required: true },
   ipAddress: { type: String },
   deviceInfo: { type: String },
 }, { timestamps: true });
 
-export const LoginHistoryModel = model("LoginHistory", LoginHistorySchema);
+export const LoginHistoryModel = model<ILogin>("LoginHistory", LoginHistorySchema);
