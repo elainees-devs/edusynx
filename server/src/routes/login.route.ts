ccf48eb18cc4754e316fca787ddfcd302d953d3c
elainees-devs <elainees.devs@gmail.com>
2025-06-07 15:34:39 +0300

    Add sanitizeHeaders middleware to login route for validating and trimming request headers

--- server/src/routes/login.route.ts
+++ server/src/routes/login.route.ts
@@ -3,11 +3,16 @@ import { Router } from "express";
 import { loginSchema } from "../validation/login.schema";
 import { LoginController } from "../controllers";
 import { validate } from "../middlewares/validate";
-
+import { sanitizeHeaders } from "../middlewares/sanitzeHeader";
 
 const loginRouter = Router();
 const loginController = new LoginController();
 
-loginRouter.post("/login", validate(loginSchema), loginController.loginUser);
+loginRouter.post(
+  "/login",
+  sanitizeHeaders,
+  validate(loginSchema),
+  loginController.loginUser
+);
 
 export { loginRouter };
