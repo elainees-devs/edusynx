f5b471f48fc6914ab0216b3e1d4768b7aea438a1
elainees-devs <elainees.devs@gmail.com>
2025-06-07 15:14:34 +0300

    fix(types): make userId nullable and add LoginFailureReason enum to ILogin interface

--- server/src/types/security/login.types.ts
+++ server/src/types/security/login.types.ts
@@ -1,13 +1,14 @@
-//src/types/security/login.types.ts
+// src/types/security/login.types.ts
 import { Types } from "mongoose";
 import { BaseDocument } from "../common/base.types";
 import { ISession } from "./session.types";
 import { IBaseUser } from "../people/user.types";
 import { ILoginBase } from "../common/auth-context.types";
+import { LoginFailureReason } from "../enum/enum";
 
 export interface ILogin extends BaseDocument, ILoginBase {
-  userId: Types.ObjectId | IBaseUser
+   userId?: Types.ObjectId | IBaseUser | null;
   sessionId?: string | ISession;
-  isSuccessful: boolean; 
-  failureReason?: string;
+  isSuccessful: boolean;
+  failureReason?: LoginFailureReason | string;
 }
