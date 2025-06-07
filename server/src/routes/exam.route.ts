15c233e9c28e2fba57ea6b396652ba75359b99b8
elainees-devs <elainees.devs@gmail.com>
2025-06-07 22:01:01 +0300

    feat(routes): add validation middleware to exam routes

--- server/src/routes/exam.route.ts
+++ server/src/routes/exam.route.ts
@@ -1,15 +1,16 @@
 // src/routes/exam.route.ts
-// src/routes/exam.route.ts
 import { Router } from "express";
 import { ExamController } from "../controllers";
+import { createExamSchema, updateExamSchema } from "../validation/exam.schema";
+import { validate } from "../middlewares/validate";
 
 const examRouter = Router();
 const examController = new ExamController();
 
-examRouter.post("/", examController.createExam);
+examRouter.post("/",validate(createExamSchema),examController.createExam);
 examRouter.get("/", examController.getAllExams);
 examRouter.get("/:id", examController.getExamById);
-examRouter.put("/:id", examController.updateExam);
+examRouter.put("/:id",validate(updateExamSchema),examController.updateExam);
 examRouter.delete("/:id", examController.deleteExam);
 examRouter.delete("/", examController.deleteAllExams);
 
