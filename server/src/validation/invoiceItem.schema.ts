7645558c7ebc66b15c9e2781dff932156355f75e
elainees-devs <elainees.devs@gmail.com>
2025-06-08 19:31:08 +0300

    refactor(invoiceItem.schema): add separate create and update schemas

--- server/src/validation/invoiceItem.schema.ts
+++ server/src/validation/invoiceItem.schema.ts
@@ -1,11 +1,13 @@
-//src/validation/invoiceItem.schema.ts
+// src/validation/invoiceItem.schema.ts
 import { z } from "zod";
 import { objectId } from "./util";
 
-export const invoiceItemSchema = z.object({
+export const createInvoiceItemSchema = z.object({
   student: objectId,
   fee: objectId,
   amount: z.number().min(0, "Amount must be a positive number"),
   originalAmount: z.number().min(0).optional(),
   description: z.string().max(1000).optional(),
 });
+
+export const updateInvoiceItemSchema = createInvoiceItemSchema.partial();
