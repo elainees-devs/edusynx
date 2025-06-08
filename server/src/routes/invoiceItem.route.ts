be02999ebc0e30634ba8c3235c894178331e3a76
elainees-devs <elainees.devs@gmail.com>
2025-06-08 19:32:12 +0300

    feat(invoiceItem.routes): add validation middleware to invoice item routes

---
+++ server/src/routes/invoiceItem.route.ts
@@ -0,0 +1,17 @@
+// src/routes/invoiceItem.routes.ts
+import { Router } from "express";
+import { InvoiceItemController } from "../controllers/invoiceItem.controller";
+import { validate } from "../middlewares/validate";
+import { createInvoiceItemSchema, updateInvoiceItemSchema } from "../validation/invoiceItem.schema";
+
+const invoiceItemRouter = Router();
+const controller = new InvoiceItemController();
+
+invoiceItemRouter.post("/",validate(createInvoiceItemSchema),controller.createInvoiceItem);
+invoiceItemRouter.get("/:id", controller.getInvoiceItemById);
+invoiceItemRouter.get("/", controller.getAllInvoiceItems);
+invoiceItemRouter.put("/:id",validate(updateInvoiceItemSchema),controller.updateInvoiceItem);
+invoiceItemRouter.delete("/:id", controller.deleteInvoiceItem);
+invoiceItemRouter.delete("/", controller.deleteAllInvoiceItems);
+
+export default invoiceItemRouter;
