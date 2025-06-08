8b8ac4f3c4c6625851b505d5241de02f20b34f0a
elainees-devs <elainees.devs@gmail.com>
2025-06-08 19:27:30 +0300

    feat(invoice.route): add validation middleware and update invoice routes with schemas

--- server/src/routes/invoice.route.ts
+++ server/src/routes/invoice.route.ts
@@ -1,14 +1,16 @@
 // src/routes/invoice.route.ts
 import { Router } from "express";
 import { InvoiceController } from "../controllers/invoice.controller";
+import { validate } from "../middlewares/validate";
+import { createInvoiceSchema, updateInvoiceSchema } from "../validation/invoice.schema";
 
 const invoiceRouter = Router();
 const invoiceController = new InvoiceController();
 
-invoiceRouter.post("/", invoiceController.createInvoice);
+invoiceRouter.post("/",validate(createInvoiceSchema), invoiceController.createInvoice);
 invoiceRouter.get("/", invoiceController.getAllInvoices);
 invoiceRouter.get("/:id", invoiceController.getInvoiceById);
-invoiceRouter.put("/:id", invoiceController.updateInvoice);
+invoiceRouter.put("/:id",validate(updateInvoiceSchema),invoiceController.updateInvoice);
 invoiceRouter.delete("/:id", invoiceController.deleteInvoice);
 invoiceRouter.delete("/", invoiceController.deleteAllInvoices);
 
