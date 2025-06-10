fab6ae9284b07aaa279a05c2f4e847e12c870253
elainees-devs <elainees.devs@gmail.com>
2025-06-10 13:59:12 +0300

    refactor(student.model): update name fields to be more explicit
    - Changed 'firstName' to 'studentFirstName'
    - Changed 'middleName' to 'studentMiddleName'
    - Changed 'lastName' to 'studentLastName'

--- server/src/models/student.model.ts
+++ server/src/models/student.model.ts
@@ -6,9 +6,9 @@ import { StudentGender, StudentStatus } from '../types/enum/enum';
 const studentSchema = new Schema<IStudent>(
   {
     school: { type: Schema.Types.ObjectId, ref: 'School', required: true },
-    firstName: { type: String, required: true },
-    middleName: { type: String, required: true },
-    lastName: { type: String, required: true },
+    studentFirstName: { type: String, required: true },
+    studentMiddleName: { type: String, required: true },
+    studentLastName: { type: String, required: true },
     studentGender: { type: String, enum: Object.values(StudentGender), required: true },
     dateOfBirth: { type: Date, required: true },
     adm: { type: Number, required: true, unique: true },
