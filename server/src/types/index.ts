14e002fa7bba4763d2acfbb2545c52e91aba7c1d
elainees-devs <elainees.devs@gmail.com>
2025-06-11 14:51:26 +0300

    chore(types): export session types and unify login response type in index.ts

--- server/src/types/index.ts
+++ server/src/types/index.ts
@@ -1,3 +1,5 @@
+import { ILoginResponseFailure, ILoginResponseSuccess } from "./auth/auth";
+
 //src/types/index.ts
 export * from "./school/school-core.types";
 export * from "./school/school-activity.types";
@@ -6,6 +8,8 @@ export * from "./people/student.types";
 export * from "./finance/finance.types";
 export * from "./enum/enum";
 export * from "./auth/auth"
+export * from "./security/session.types"
+export type ILoginResponse = ILoginResponseSuccess | ILoginResponseFailure;
 
 
 
