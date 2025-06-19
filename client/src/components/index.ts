48594ad691376f7b29152c9f7e04126602df1d74
elainees-devs <elainees.devs@gmail.com>
2025-06-19 14:45:13 +0300

    chore(components): export KeyFetures from components index

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -2,5 +2,6 @@
 
 export { default as NavBar } from "./navbar";
 export { default as Hero } from "./hero";
+export { default as KeyFetures } from "./key-features";
 export { default as HowItWorks } from "./how-it-works";
 export {default as PricingSection} from "./pricing-section"
