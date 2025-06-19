0b9a3d63dbffc80212e767d9116eae1b332b651b
elainees-devs <elainees.devs@gmail.com>
2025-06-19 15:04:15 +0300

    chore: add Benefits component to exports in index.ts

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -3,5 +3,6 @@
 export { default as NavBar } from "./navbar";
 export { default as Hero } from "./hero";
 export { default as KeyFetures } from "./key-features";
+export { default as Benefits } from "./benefits";
 export { default as HowItWorks } from "./how-it-works";
 export {default as PricingSection} from "./pricing-section"
