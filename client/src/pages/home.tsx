8653236023d7fe78cd437185a4ba483f234be20a
elainees-devs <elainees.devs@gmail.com>
2025-06-19 13:25:07 +0300

    feat: include PricingSection in HomePage layout

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,5 +1,5 @@
 // src/pages/home.tsx
-import { Hero, NavBar } from "../components"; "../components/index";
+import { Hero, NavBar, PricingSection } from "../components"; "../components/index";
 import Footer from "../shared/layout/footer";
 
 const HomePage = () => {
@@ -7,6 +7,7 @@ const HomePage = () => {
     <>
     <NavBar />
     <Hero />
+    <PricingSection />
     <Footer />
     </>
   );
