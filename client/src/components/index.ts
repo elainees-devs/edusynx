d74a03b5bb552ff3d79612c6aee69574307009f6
elainees-devs <elainees.devs@gmail.com>
2025-06-19 14:23:05 +0300

    chore(components): export HowItWorks component in index

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -2,4 +2,5 @@
 
 export { default as NavBar } from "./navbar";
 export { default as Hero } from "./hero";
+export { default as HowItWorks } from "./how-it-works";
 export {default as PricingSection} from "./pricing-section"
