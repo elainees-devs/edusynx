7c404f0da5c89d0333537199df734ae8ef613399
elainees-devs <elainees.devs@gmail.com>
2025-06-20 20:02:33 +0300

    feat: add basic School Admin dashboard page

---
+++ client/src/pages/dashboard/school-admin.tsx
@@ -0,0 +1,9 @@
+// src/pages/dashboard/school-admin.tsx
+const SchoolAdminDashBoard: React.FC = () =>{
+    return(
+        <div>
+            <h1>School Admin Dashboard</h1>
+        </div>
+    )
+}
+export default SchoolAdminDashBoard
