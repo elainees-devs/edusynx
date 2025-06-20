4acb12d5ea8f740cda13b0fc9d6065cf43371d88
elainees-devs <elainees.devs@gmail.com>
2025-06-20 17:32:14 +0300

    feat(pages): add SignIn page with LoginForm component

---
+++ client/src/pages/signin.tsx
@@ -0,0 +1,15 @@
+// src/pages/signin.tsx
+
+import { LoginForm } from "../components";
+
+const SignIn: React.FC = () => {
+  return (
+    <div className="h-screen p-16 bg-light">
+      <LoginForm />
+    </div>
+  );
+};
+
+export default SignIn;
+
+
