f663d084f45b7d58db5fd18fd339b4dcef9a69b2
elainees-devs <elainees.devs@gmail.com>
2025-06-20 13:42:42 +0300

    feat: export SignUp page from pages index

---
+++ client/src/pages/index.ts
@@ -0,0 +1,8 @@
+// src/pages/index.ts
+// Top-level pages
+export { default as HomePage } from './home';
+export { default as SignIn} from './signin';
+export { default as SignUp} from './signup';
+
+// export { default as ForgotPasswordPage } from './forgot-password';
+// export { default as ResetPasswordPage } from './reset-password';
