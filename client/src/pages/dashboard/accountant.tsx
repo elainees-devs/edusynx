0bfd3e4bbc9a085522f773d699286b4812266fa5
elainees-devs <elainees.devs@gmail.com>
2025-06-20 20:00:25 +0300

    feat: add basic Accountant dashboard page

---
+++ client/src/pages/dashboard/accountant.tsx
@@ -0,0 +1,9 @@
+// src/pages/dashboard/accountant.tsx
+const AccountantDashBoard: React.FC = () =>{
+    return(
+        <div>
+            <h1>Accountant Dashboard</h1>
+        </div>
+    )
+}
+export default AccountantDashBoard
