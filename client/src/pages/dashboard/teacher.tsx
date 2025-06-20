110443efb66004d75e903c09435e74d47bb0607c
elainees-devs <elainees.devs@gmail.com>
2025-06-20 20:01:41 +0300

    feat: add basic Teacher dashboard page

---
+++ client/src/pages/dashboard/teacher.tsx
@@ -0,0 +1,9 @@
+// src/pages/dashboard/teacher.tsx
+const TeacherDashBoard: React.FC = () =>{
+    return(
+        <div>
+            <h1>Teacher Dashboard</h1>
+        </div>
+    )
+}
+export default TeacherDashBoard
