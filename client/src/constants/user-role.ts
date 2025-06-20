e5b600103004bc9b21048ecf2ed38683796c8c9d
elainees-devs <elainees.devs@gmail.com>
2025-06-20 17:21:23 +0300

    feat(constants): define UserRole enum and corresponding type

---
+++ client/src/constants/user-role.ts
@@ -0,0 +1,10 @@
+// src/constants/user-role.ts
+export const UserRole = {
+  HEADTEACHER: "headteacher",
+  TEACHER: "teacher",
+  SCHOOL_ADMIN: "school-admin",
+  GUARDIAN: "guardian",
+  ACCOUNTANT: "accountant",
+} as const;
+
+export type UserRole = typeof UserRole[keyof typeof UserRole];
