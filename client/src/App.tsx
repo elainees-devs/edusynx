2be697dbbf5cb5899b226f1f6200b7fd082038fa
elainees-devs <elainees.devs@gmail.com>
2025-06-20 20:14:51 +0300

    feat: add dashboard routes to App component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,14 @@
 // src/App.tsx
-import './index.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { HomePage, ResetPassword, SignIn, SignUp } from './pages';
-
+import "./index.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HomePage, ResetPassword, SignIn, SignUp } from "./pages";
+import {
+  AccountantDashboard,
+  GuardianDashboard,
+  HeadTeacherDashboard,
+  SchoolAdminDashboard,
+  TeacherDashboard,
+} from "./pages/dashboard/";
 
 function App() {
   return (
@@ -12,10 +18,16 @@ function App() {
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/reset-password" element={<ResetPassword />} />
+        
+        {/* Dashboards */}
+        <Route path="/teacher" element={<TeacherDashboard />} />
+        <Route path="/headteacher" element={<HeadTeacherDashboard />} />
+        <Route path="/guardian" element={<GuardianDashboard />} />
+        <Route path="/accountant" element={<AccountantDashboard />} />
+        <Route path="/school-admin" element={<SchoolAdminDashboard />} />
       </Routes>
     </Router>
   );
 }
 
 export default App;
-
