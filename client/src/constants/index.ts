43c8f9bf1ff7540a0d37e17f231777645b41e844
elainees-devs <elainees.devs@gmail.com>
2025-06-20 17:23:03 +0300

    chore(constants): export UserRole and UserRoleType from index

--- client/src/constants/index.ts
+++ client/src/constants/index.ts
@@ -6,3 +6,8 @@ export * from './keyFeatures'
 export * from './benefits';
 export * from './pricingPlans';
 export * from './steps';
+
+//export user roles
+export { UserRole } from './user-role'
+export type { UserRole as UserRoleType } from './user-role';
+
