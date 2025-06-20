132fce3a328487e5fb840b75c17a2ce8ac415dee
elainees-devs <elainees.devs@gmail.com>
2025-06-20 19:31:50 +0300

    chore: update App.tsx to import global styles from index.css instead of App.css

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 // src/App.tsx
-import './App.css';
+import './index.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { HomePage, ResetPassword, SignIn, SignUp } from './pages';
 
