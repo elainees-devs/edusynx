a176cfb5e4faa970cfc1be2ce763609663cbd2c9
elainees-devs <elainees.devs@gmail.com>
2025-06-20 20:03:19 +0300

    feat: add basic Guardian dashboard page

---
+++ client/src/pages/dashboard/guardian.tsx
@@ -0,0 +1,9 @@
+// src/pages/dashboard/guardian.tsx
+const GuardianDashBoard: React.FC = () =>{
+    return(
+        <div>
+            <h1>Guardian Dashboard</h1>
+        </div>
+    )
+}
+export default GuardianDashBoard
