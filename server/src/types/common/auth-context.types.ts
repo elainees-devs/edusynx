c15f7571ae5aaf100d5fca0b0cc1b4050711a8ba
elainees-devs <elainees.devs@gmail.com>
2025-06-21 09:11:04 +0300

    refactor: extend ILoginBase to include userAgent and location details

--- server/src/types/common/auth-context.types.ts
+++ server/src/types/common/auth-context.types.ts
@@ -1,6 +1,12 @@
-//src/types/common/auth-context.types.ts
+// src/types/common/auth-context.types.ts
 export interface ILoginBase {
   loginTime: Date;
   ipAddress?: string;
   deviceInfo?: string;
+  userAgent?: string;
+  location?: {
+    country?: string;
+    region?: string;
+    city?: string;
+  };
 }
