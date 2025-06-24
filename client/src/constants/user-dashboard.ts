428b6a089db1107e9338a77217dcdeb802f33a3b
elainees-devs <elainees.devs@gmail.com>
2025-06-24 11:13:13 +0300

    feat: define userDashboard role-path mapping

---
+++ client/src/constants/user-dashboard.ts
@@ -0,0 +1,8 @@
+// client/src/constants/user-dashboard.ts
+export const userDashboard = [
+  { role: "ADMIN", path: "/dashboard/super-admin" },
+  { role: "SCHOOL-ADMIN", path: "/dashboard/school-admin" },
+  { role: "HEADTEACHER", path: "/dashboard/headteacher" },
+  { role: "GUARDIAN", path: "/dashboard/guardian" },
+  { role: "ACCOUNTANT", path: "/dashboard/accountant" },
+];
