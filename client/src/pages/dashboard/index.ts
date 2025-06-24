5b8889ae8370d168b09bbbb0f8a961c9c920b3a1
elainees-devs <elainees.devs@gmail.com>
2025-06-24 18:57:17 +0300

    chore: update SuperAdminDashboard and HeadTeacherDashboard paths in dashboard index

--- client/src/pages/dashboard/index.ts
+++ client/src/pages/dashboard/index.ts
@@ -1,6 +1,7 @@
-// src/pages/dashboard/index.ts
+// client/src/pages/dashboard/index.ts
 export {default as AccountantDashboard} from './accountant'
 export {default as TeacherDashboard} from './teacher'
 export {default as GuardianDashboard} from './guardian'
-export {default as HeadTeacherDashboard} from './headteacher'
+export {default as HeadTeacherDashboard} from '../headteacher'
 export {default as SchoolAdminDashboard} from './school-admin'
+export {default as SuperAdminDashboard} from '../super-admin'
