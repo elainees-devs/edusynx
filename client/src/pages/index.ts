7f2542437783d6b5ad9f36d2be13435ade44eba4
elainees-devs <elainees.devs@gmail.com>
2025-06-24 14:51:02 +0300

    feat(pages): replace SignIn export with SchoolRegistrationPage in index

--- client/src/pages/index.ts
+++ client/src/pages/index.ts
@@ -2,5 +2,5 @@
 // Top-level pages
 export { default as HomePage } from './home';
 export { default as SignIn} from './signin';
-export { default as SignUp} from './signup';
+export { default as SchoolRegistrationPage} from './school-register';
 export { default as ResetPassword } from './reset-password';
