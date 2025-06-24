77a45ab4551d35e8eac27559c06288dd152732e8
elainees-devs <elainees.devs@gmail.com>
2025-06-24 14:43:47 +0300

    refactor(route): reorganize and update school routes
    - Changed POST route from '/' to '/register'
    - Moved dynamic GET ':id' route to the end for proper route resolution
    - Updated PATCH route to ':id/activate' from '/schools/:id/activate'
    - Added ObjectId validation middleware for ':id' in GET route

--- server/src/routes/school.route.ts
+++ server/src/routes/school.route.ts
@@ -1,4 +1,4 @@
-// src/routes/school.route.ts
+// server/src/routes/school.route.ts
 import { Router } from "express";
 import { validate } from "../middlewares/validate";
 import {
@@ -6,24 +6,28 @@ import {
   updateSchoolSchema,
 } from "../validation/school.schema";
 import { SchoolController } from "../controllers";
+import { validateObjectId } from "../middlewares/validateObjectId";
 
 const schoolRouter = Router();
 const schoolController = new SchoolController();
 
+// Specific routes first
 schoolRouter.post(
-  "/",
+  "/register",
   validate(createSchoolSchema),
   schoolController.createSchool
 );
-schoolRouter.get("/:id", schoolController.getSchoolById);
 schoolRouter.get("/", schoolController.getAllSchools);
+schoolRouter.patch(":id/activate", schoolController.activateSchool);
 schoolRouter.put(
   "/:id",
   validate(updateSchoolSchema),
   schoolController.updateSchool
 );
-schoolRouter.patch("/schools/:id/activate", schoolController.activateSchool);
 schoolRouter.delete("/:id", schoolController.deleteSchoolById);
 schoolRouter.delete("/", schoolController.deleteAllSchools);
 
-export {schoolRouter}
+// Dynamic route last
+schoolRouter.get("/:id", validateObjectId("id"), schoolController.getSchoolById);
+
+export { schoolRouter };
