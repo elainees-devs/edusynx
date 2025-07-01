d7001749f4184af7afcbcae2d49e07f70b12aa13
Elaine Muhombe <elainees.devs@gmail.com>
2025-07-01 17:44:10 +0300

    feat: add signup route and simplify route elements in App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,13 @@
 // client/src/App.tsx
 import "./index.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { HomePage, ResetPassword, SignIn, SchoolRegistrationPage } from "./pages";
+import {
+  HomePage,
+  ResetPassword,
+  SignIn,
+  SchoolRegistrationPage,
+  SignUp,
+} from "./pages";
 import {
   AccountantDashboard,
   GuardianDashboard,
@@ -12,36 +18,40 @@ import {
 } from "./pages/dashboard/";
 import { GlobalStateProvider } from "./context/useGlobalState";
 
-
-
 function App() {
   return (
     <GlobalStateProvider>
-       <Router>
-      <Routes>
-        {/* Pages */}
-        <Route path="/" element={<HomePage />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route
-          path="/register"
-          element={
-            <SchoolRegistrationPage />
-             
-          }
-        />
-        <Route path="/reset-password" element={<ResetPassword />} />
-        
-        {/* Dashboards */}
-        <Route path="/dashboard/teacher" element={<TeacherDashboard />} />
-        <Route path="/dashboard/headteacher" element={<HeadTeacherDashboard />} />
-        <Route path="/dashboard/guardian" element={<GuardianDashboard />} />
-        <Route path="/dashboard/accountant" element={<AccountantDashboard />} />
-        <Route path="/dashboard/school-admin" element={<SchoolAdminDashboard />} />
-           <Route path="/dashboard/super-admin" element={<SuperAdminDashboard />} />
-      </Routes>
-    </Router>
+      <Router>
+        <Routes>
+          {/* Pages */}
+          <Route path="/" element={<HomePage />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/:slug/signup" element={<SignUp />} />
+          <Route path="/register" element={<SchoolRegistrationPage />} />
+          <Route path="/reset-password" element={<ResetPassword />} />
+
+          {/* Dashboards */}
+          <Route path="/dashboard/teacher" element={<TeacherDashboard />} />
+          <Route
+            path="/dashboard/headteacher"
+            element={<HeadTeacherDashboard />}
+          />
+          <Route path="/dashboard/guardian" element={<GuardianDashboard />} />
+          <Route
+            path="/dashboard/accountant"
+            element={<AccountantDashboard />}
+          />
+          <Route
+            path="/dashboard/school-admin"
+            element={<SchoolAdminDashboard />}
+          />
+          <Route
+            path="/dashboard/super-admin"
+            element={<SuperAdminDashboard />}
+          />
+        </Routes>
+      </Router>
     </GlobalStateProvider>
-   
   );
 }
 
