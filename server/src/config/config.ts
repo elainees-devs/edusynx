ea6b71249163187a6db72d2110c00e14e5e2eaf7
Elaine Muhombe <elainees.devs@gmail.com>
2025-07-01 17:19:19 +0300

    chore(config): set default values for frontend URL, server port, and MongoDB URI

---
+++ server/src/config/config.ts
@@ -0,0 +1,4 @@
+// server/src/config/config.ts
+export const FRONTEND_BASE_URL = process.env.FRONTEND_BASE_URL || "http://localhost:5173";
+export const PORT = process.env.PORT || 5000;
+export const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/edusynxDB";
