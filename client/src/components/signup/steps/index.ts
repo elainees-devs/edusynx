23a7dc1eb3dc9fb40b53312ebab8a5ae1d29ce7e
Elaine Muhombe <elainees.devs@gmail.com>
2025-07-01 22:35:21 +0300

    chore(signup): add index file to export signup step components

---
+++ client/src/components/signup/steps/index.ts
@@ -0,0 +1,5 @@
+// client/src/components/signup/steps/index.ts
+export { default as PersonalInfoStep } from './personal-info';
+export { default as ContactInfoStep } from './contact-info';
+export { default as AccountInfoStep } from './account-info';
+export { default as RoleSpecificStep } from './role-specific';
