f29751a64fbe73f28f8b18ee10fe1d58fe12c99c
elainees-devs <elainees-devs@gmail.com>
2025-07-08 21:48:02 +0300

    Refactor: Move super-admin.tsx to dashboard/

---
+++ client/src/pages/dashboard/super-admin.tsx
@@ -0,0 +1,14 @@
+import Sidebar from "../../shared/layout/dashboard/sidebar"
+import Topbar from "../../shared/layout/dashboard/topbar"
+
+const SuperAdminDashboard: React.FC = () =>{
+    return(
+        <div>
+            <Topbar />
+            <Sidebar />
+           
+        </div>
+    )
+}
+
+export default SuperAdminDashboard
