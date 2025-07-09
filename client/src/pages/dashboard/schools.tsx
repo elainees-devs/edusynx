bf10142ecedf282d6b57d515a7b680232cd4a47b
elainees-devs <elainees-devs@gmail.com>
2025-07-09 18:25:04 +0300

    feat(super-admin): create Schools page and render SchoolTable component

---
+++ client/src/pages/dashboard/schools.tsx
@@ -0,0 +1,11 @@
+import SchoolTable from "../../components/dashboard/super-admin/school-list";
+
+const Schools: React.FC = () => {
+  return (
+    <div>
+      <div className="bg-white p-6 rounded-lg shadow-md"></div>
+      <SchoolTable />
+    </div>
+  );
+};
+export default Schools;
