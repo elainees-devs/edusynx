2dd5910e40d2a3936094c5b86728962b3d1585e1
elainees-devs <elainees-devs@gmail.com>
2025-07-10 22:18:32 +0300

    feat: add SuperAdmin routes for login and creation

---
+++ server/src/routes/super-admin.route.ts
@@ -0,0 +1,15 @@
+// server/src/routes/super-admin.route.ts
+
+import { Router } from "express";
+import { SuperAdminController } from "../controllers/super-admin.controller";
+
+const adminRouter = Router();
+const superAdminController = new SuperAdminController();
+
+
+adminRouter.post("/login", superAdminController.login);
+
+
+adminRouter.post("/create", superAdminController.createSuperAdmin);
+
+export default adminRouter;
