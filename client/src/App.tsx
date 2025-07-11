61c65c12f53cbb3ebd62b06b871f9b4fea0901af
elainees-devs <elainees-devs@gmail.com>
2025-07-11 16:54:13 +0300

    feat: add route for Super Admin signup page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,6 +8,7 @@ import {
   SchoolRegistrationPage,
   SignUp,
   Schools,
+  SuperAdminSignUp,
 } from "./pages";
 import {
   AccountantDashboard,
@@ -28,6 +29,7 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/:slug/signin" element={<SignIn />} />
           <Route path="/:slug/signup" element={<SignUp />} />
+          <Route path="/super-admin" element={<SuperAdminSignUp />} />
           <Route path="/register" element={<SchoolRegistrationPage />} />
           <Route path="/reset-password" element={<ResetPassword />} />
 
