b209a319ec14fe10622b04f974d5b720fba5b4fe
elainees-devs <elainees-devs@gmail.com>
2025-07-11 16:55:21 +0300

    feat: export SuperAdminSignUp component from index

--- client/src/pages/index.ts
+++ client/src/pages/index.ts
@@ -3,6 +3,7 @@
 export { default as HomePage } from './home';
 export { default as SignIn} from './signin';
 export { default as SignUp } from './signup';
+export { default as SuperAdminSignUp } from './signup/super-admin-signup';
 export { default as SchoolRegistrationPage} from './school-register';
 export { default as ResetPassword } from './reset-password';
 export { default as Schools } from  './dashboard/schools';
