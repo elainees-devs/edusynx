b3ba5dbdda102ad90c7264dcdb4d5c4d7d2ccfde
elainees-devs <elainees-devs@gmail.com>
2025-07-12 20:45:01 +0300

    refactor: replace 'any' with 'unknown' in cn util

--- client/src/utils/cn.ts
+++ client/src/utils/cn.ts
@@ -1,6 +1,6 @@
-// src/utils/cn.ts
+// client/src/utils/cn.ts
 import { clsx } from "clsx";
 
-export function cn(...inputs: any[]) {
+export function cn(...inputs: unknown[]) {
   return clsx(inputs);
 }
