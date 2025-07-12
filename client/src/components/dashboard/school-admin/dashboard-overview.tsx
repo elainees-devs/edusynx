b84ce3d12ddf66723b0bc78d570983c8a2b96b54
elainees-devs <elainees-devs@gmail.com>
2025-07-12 19:47:27 +0300

    feat: Add DashboardOverview component for school admin

---
+++ client/src/components/dashboard/school-admin/dashboard-overview.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SchoolAdminDashboardOverview: React.FC = () =>{
+    return(
+        <div>School Admin Dashboard Overview</div>
+    )
+}
+
+export default SchoolAdminDashboardOverview;
