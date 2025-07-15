2cc800a8c1d0e567678040fe03f8641f5bef4fe3
elainees-devs <elainees.devs@gmail.com>
2025-07-15 10:08:14 +0300

    :reorder: Move 'Schools' export to group with dashboard pages

--- client/src/pages/index.ts
+++ client/src/pages/index.ts
@@ -1,9 +1,9 @@
 // src/pages/index.ts
-// Top-level pages
+
 export { default as HomePage } from './home';
 export { default as SignIn} from './signin';
 export { default as SignUp } from './signup';
 export { default as SuperAdminSignUp } from './signup/super-admin-signup';
 export { default as SchoolRegistrationPage} from './school-register';
-export { default as ResetPassword } from './reset-password';
 export { default as Schools } from  './dashboard/schools';
+export { default as ResetPassword } from  './reset-password';
