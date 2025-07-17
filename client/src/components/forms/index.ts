e803ffbb7179c3bc30a0d046a4acde741f40c303
elainees-devs <elainees.devs@gmail.com>
2025-07-17 19:00:13 +0300

    feat(forms): export SuperAdmin login and signup forms from index

---
+++ client/src/components/forms/index.ts
@@ -0,0 +1,5 @@
+// client/src/components/forms/index.ts
+export { default as LoginForm } from "./login-form";
+export {default as SuperAdminLoginForm} from "./super-admin-login-form";
+export {default as ForgotPasswordForm } from "./forgot-password-form";
+export {default as SuperAdminSignUpForm} from "./super-admin-signup-form";
