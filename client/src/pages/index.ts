d3d177cb9e83ab82105da9cfb717415f631e8db1
elainees-devs <elainees.devs@gmail.com>
2025-07-17 18:42:04 +0300

    refactor(pages): update exports for SuperAdminSignIn and adjust SignIn import paths

--- client/src/pages/index.ts
+++ client/src/pages/index.ts
@@ -1,8 +1,9 @@
 // src/pages/index.ts
 
 export { default as HomePage } from './home';
-export { default as SignIn} from './signin';
-export { default as SignUp } from './signup';
+export { default as SignIn} from './signin/signin';
+export { default as SuperAdminSignIn } from './signin/super-admin-signin';
+export { default as SignUp } from './signup/signup';
 export { default as SuperAdminSignUp } from './signup/super-admin-signup';
 export { default as SchoolRegistrationPage} from './school-register';
 export { default as Schools } from  './dashboard/schools';
