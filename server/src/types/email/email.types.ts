c8f45c18e37c89be0496d797b13387f2ed0cd279
elainees-devs <elainees.devs@gmail.com>
2025-07-18 09:07:19 +0300

    feat(types): add SendAccessLinkBody and SendResetTokenBody interfaces for email module

---
+++ server/src/types/email/email.types.ts
@@ -0,0 +1,10 @@
+// server/src/types/email/email.types.ts
+export interface SendAccessLinkBody {
+  email: string;
+  accessUrl: string;
+}
+
+export interface SendResetTokenBody {
+  email: string;
+  token: string;
+}
