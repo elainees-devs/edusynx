7d0d4317f2c5d354a221601c0cea734dadb85e76
elainees-devs <elainees.devs@gmail.com>
2025-07-18 10:05:26 +0300

    refactor(forms): replace ForgotPasswordForm export with ResetPasswordForm in index

--- client/src/components/forms/index.ts
+++ client/src/components/forms/index.ts
@@ -1,5 +1,5 @@
 // client/src/components/forms/index.ts
 export { default as LoginForm } from "./login-form";
 export {default as SuperAdminLoginForm} from "./super-admin-login-form";
-export {default as ForgotPasswordForm } from "./reset-password-form";
+export {default as ResetPasswordForm } from "./reset-password-form";
 export {default as SuperAdminSignUpForm} from "./super-admin-signup-form";
