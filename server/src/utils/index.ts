bde379b315151abc6d6811a1a17560587774327d
elainees-devs <elainees.devs@gmail.com>
2025-07-19 15:38:16 +0300

    chore(utils): export userEmail utility from index.ts

---
+++ server/src/utils/index.ts
@@ -0,0 +1,8 @@
+// server/src/utils/index.ts
+export * from "./handleAsync";
+export * from "./AppError";
+export * from "./tokenGenerator";
+export * from "./imageOptimization";
+export * from "./logger";
+export * from "./slugify";
+export * from "./userEmail"
