1e2c2d2725256498718597f458f75fb0a3d37b61
elainees-devs <elainees.devs@gmail.com>
2025-07-19 14:57:44 +0300

    style(signup): change PersonalInfoStep Next button color to green

--- client/src/components/signup/steps/personal-info.tsx
+++ client/src/components/signup/steps/personal-info.tsx
@@ -30,7 +30,7 @@ const PersonalInfoStep = ({ next }: { next: () => void }) => {
         value={formData.nationality || ''}
         onChange={(e) => updateForm({ nationality: e.target.value })}
       />
-      <button onClick={next} className="px-4 py-2 mt-4 text-white bg-blue-600 rounded">Next</button>
+      <button onClick={next} className="px-4 py-2 mt-4 text-white bg-green-600 rounded">Next</button>
     </div>
   );
 };
