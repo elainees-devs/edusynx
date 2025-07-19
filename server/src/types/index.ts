7410a96954688fef288915ae3849e77264ce1f8f
elainees-devs <elainees.devs@gmail.com>
2025-07-19 15:34:46 +0300

    fix(types): correct auth import in index.ts and define ILoginResponse union type

--- server/src/types/index.ts
+++ server/src/types/index.ts
@@ -1,13 +1,13 @@
-import { ILoginResponseFailure, ILoginResponseSuccess } from "./auth/auth";
+import { ILoginResponseFailure, ILoginResponseSuccess } from "./auth/auth.types";
 
-//src/types/index.ts
+// server/src/types/index.ts
 export * from "./school/school-core.types";
 export * from "./school/school-activity.types";
 export * from "./people/user.types";
 export * from "./people/student.types";
 export * from "./finance/finance.types";
 export * from "./enum/enum";
-export * from "./auth/auth"
+export * from "./auth/auth.types"
 export * from "./security/session.types"
 export * from "./email/email.types"
 export type ILoginResponse = ILoginResponseSuccess | ILoginResponseFailure;
