9119c6a71a6a4fd8ea215b8e2d215cf6c4a1b1e3
elainees-devs <elainees.devs@gmail.com>
2025-07-19 20:23:49 +0300

    feat: add NewPassword export to pages/index.ts for routing

--- client/src/pages/index.ts
+++ client/src/pages/index.ts
@@ -7,3 +7,4 @@ export { default as SuperAdminSignUp } from './signup/super-admin-signup';
 export { default as SchoolRegistrationPage} from './school-register';
 export { default as Schools } from  './dashboard/schools';
 export { default as ResetPassword } from  './password/reset-password';
+export { default as NewPassword } from  './password/new-password';
