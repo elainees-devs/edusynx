6d078ba9ede9c0d57afa721ad9d212a138edc89a
elainees-devs <elainees.devs@gmail.com>
2025-07-20 20:56:48 +0300

    feat: export ViewClass page from pages index file

--- client/src/pages/index.ts
+++ client/src/pages/index.ts
@@ -9,3 +9,4 @@ export { default as Schools } from  './dashboard/schools';
 export { default as ResetPassword } from  './password/reset-password';
 export { default as NewPassword } from  './password/new-password';
 export { default as RegisterClass} from './register-class';
+export { default as ViewClass} from './view-class';
