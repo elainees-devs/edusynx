c7801b46ea3da268becef7dc80cfbac08495a6c0
elainees-devs <elainees.devs@gmail.com>
2025-07-20 19:24:43 +0300

    fix: change SUPER_ADMIN assignment from = to :

--- client/src/constants/user-role.ts
+++ client/src/constants/user-role.ts
@@ -5,6 +5,7 @@ export const UserRole = {
   SCHOOL_ADMIN: "school-admin",
   GUARDIAN: "guardian",
   ACCOUNTANT: "accountant",
+  SUPER_ADMIN:"super-admin"
 } as const;
 
 export type UserRole = typeof UserRole[keyof typeof UserRole];
