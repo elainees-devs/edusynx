4fc0993929b1f6b18a6f89b64581251d1f2f0908
elainees-devs <elainees.devs@gmail.com>
2025-07-22 15:59:45 +0300

    chore: export ViewTeachersDetails component from components index

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -7,4 +7,5 @@ export { default as HowItWorks } from "./how-it-works";
 export {default as PricingSection} from "./pricing-section"
 export { default as LoginForm } from "./forms/login-form";
 export { default as ClassDetails } from "./class/class-details";
+export { default as ViewTeachersDetails } from "./teachers/view-teachers-details";
 
