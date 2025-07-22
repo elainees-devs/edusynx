92c8c71d16cedbb3f80b8daac6f2d6b6a26f8b8d
elainees-devs <elainees.devs@gmail.com>
2025-07-22 21:25:04 +0300

    chore: export Topbar, Sidebar, and DashboardLayout from dashboard index

---
+++ client/src/shared/layout/dashboard/index.ts
@@ -0,0 +1,4 @@
+// client/src/shared/layout/dashboard/index.ts
+export { default as Topbar} from "./topbar";
+export { default as Sidebar } from "./sidebar";
+export { default as DashboardLayout} from "./dashboard-layout";
