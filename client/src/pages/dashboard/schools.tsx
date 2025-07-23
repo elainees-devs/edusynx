6ea0bb5e307767f0f190a13b495e341fb70cc77b
elainees-devs <elainees.devs@gmail.com>
2025-07-23 15:57:09 +0300

    feat(schools-page): add back button with icon and navigation

--- client/src/pages/dashboard/schools.tsx
+++ client/src/pages/dashboard/schools.tsx
@@ -1,11 +1,30 @@
+// client/src/pages/dashboard/schools.tsx
+import { useNavigate } from "react-router-dom";
 import SchoolTable from "../../components/dashboard/super-admin/school-list";
+import { FiArrowLeft } from "react-icons/fi";
+
 
 const Schools: React.FC = () => {
+  const navigate = useNavigate();
+
+  const handleNavigate = () =>{
+    navigate(-1)
+  }
   return (
     <div>
-      <div className="bg-white p-6 rounded-lg shadow-md"></div>
-      <SchoolTable />
-    </div>
+        <button
+        onClick={handleNavigate}
+        className="flex items-center gap-1 p-2 text-sm text-teal-600 hover:underline"
+        aria-label="Go back"
+      >
+        <FiArrowLeft className="text-lg" />
+        Back
+      </button>
+  
+            <SchoolTable />
+          </div>
+
   );
 };
+
 export default Schools;
