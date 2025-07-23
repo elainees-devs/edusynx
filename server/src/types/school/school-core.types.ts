61c9fb1096038af8f6580c00d476d6c88d65c44d
elainees-devs <elainees.devs@gmail.com>
2025-07-23 21:30:00 +0300

    feat(types): add IStream interface to school-core.types for stream data modeling

--- server/src/types/school/school-core.types.ts
+++ server/src/types/school/school-core.types.ts
@@ -1,39 +1,41 @@
 // server/src/types/school/school-core.types.ts
-
 import mongoose, { Types } from "mongoose";
 import { BaseDocument } from "../common/base.types";
 import { IBaseUser } from "../people/user.types";
 
 export interface ISchool extends BaseDocument {
-  name: string;
-  address: string;
-  phoneNumber: string;
-  email: string;
-  website?: string;
-  establishedYear: number;
-  logoUrl?: string;
-  isActive: boolean;
-  schoolCode: string;
-  slug?: string;
-  role: string;
-  accessUrl?: string;
+  name: string
+  address: string
+  phoneNumber: string
+  email: string
+  website?: string
+  establishedYear: number
+  logoUrl?: string
+  isActive: boolean
+  schoolCode: string
+  slug?: string
+  role: string
+  accessUrl?: string
 }
-
+export interface Istream extends BaseDocument{
+  school: Types.ObjectId | ISchool
+  stream: string
+}
 export interface IClass extends BaseDocument {
-  school: Types.ObjectId | ISchool;
-  className: string;
-  stream: string;
-  academicYear: string;
+  school: Types.ObjectId | ISchool
+  className: string
+  stream: string
+  academicYear: string
 }
 
 export interface ISubject extends BaseDocument {
-  school: Types.ObjectId | ISchool;
-  subjectName: string;
-  classRef: Types.ObjectId | IClass;
+  school: Types.ObjectId | ISchool
+  subjectName: string
+  classRef: Types.ObjectId | IClass
 }
 
 export interface ITeacherSubject extends BaseDocument {
   school: Types.ObjectId | ISchool;
-  teacherId: Types.ObjectId | IBaseUser;
-  subjectId: Types.ObjectId | ISubject;
+  teacherId: Types.ObjectId | IBaseUser
+  subjectId: Types.ObjectId | ISubject
 }
