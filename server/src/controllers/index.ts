aebf0dbccf14c376080064c020d61c3715d02560
elainees-devs <elainees.devs@gmail.com>
2025-07-24 15:13:17 +0300

    feat(controller): export stream.controller in index

--- server/src/controllers/index.ts
+++ server/src/controllers/index.ts
@@ -20,3 +20,4 @@ export * from "./rolePermission.controller";
 export * from "./user.controller"
 export * from "./event.controller"
 export * from "./allocation.controller"
+export * from "./stream.controller"
