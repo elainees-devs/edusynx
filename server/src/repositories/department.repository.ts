37ce0ee0bd7a2723236eaabe22fb38d653cb796f
elainees-devs <elainees.devs@gmail.com>
2025-07-25 20:30:25 +0300

    feat(repository): Implement full CRUD operations for DepartmentRepository with school population

---
+++ server/src/repositories/department.repository.ts
@@ -0,0 +1,42 @@
+// server/src/repositories/department.repository.ts
+import { CreateDepartmentDTO } from "../dto";
+import { DepartmentModel } from "../models/department.model";
+import { IDepartment } from "../types";
+
+// Repository for managing department-related database operations
+export class DepartmentRepository{
+    // Add new department
+    async createDepartment(departmentData:CreateDepartmentDTO):Promise<IDepartment>{
+        const departmentInstance = new DepartmentModel(departmentData)
+        return await departmentInstance.save()
+}
+
+    // Retrieve department details by ID
+    async getDepartmentById(departmentId: string):Promise<IDepartment | null>{
+        return await DepartmentModel.findById(departmentId).populate("school")
+}
+
+    // Retrieve all departments
+    async getAllDepartments(): Promise<IDepartment[]>{
+        return await DepartmentModel.find().populate("school")
+}
+
+    // Update department details
+    async updateDepartment(
+        departmentId: string,
+        departmentData:Partial<CreateDepartmentDTO>):Promise<IDepartment | null>{
+        return await DepartmentModel.findByIdAndUpdate(departmentId, departmentData,{
+            new: true,
+        }).populate("school")
+}
+
+    // Delete department by Id
+    async deleteDepartment(departmentId: string): Promise<IDepartment | null>{
+        return await DepartmentModel.findByIdAndDelete(departmentId).populate("school")
+    }
+
+    // Delete all departments
+    async deleteAllDepartments(): Promise<void>{
+        await DepartmentModel.deleteMany({})
+    }
+}
