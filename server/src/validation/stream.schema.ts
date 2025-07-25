d8983f7d4ebe2e3cb80dce5dd2ed4cfe1f6508b0
elainees-devs <elainees.devs@gmail.com>
2025-07-25 19:28:39 +0300

    feat(validation): Add academicYear to stream validation schema

--- server/src/validation/stream.schema.ts
+++ server/src/validation/stream.schema.ts
@@ -5,6 +5,7 @@ import { objectId } from "./util";
 export const createStreamSchema = z.object({
   school: objectId,
   streamName: z.string().min(3),
+  academicYear: z.string().min(3),
 });
 
 export const updateStreamSchema = createStreamSchema.partial();
