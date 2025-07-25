7a181ff4925fba6be8de3438858e34d14e8e8e45
elainees-devs <elainees.devs@gmail.com>
2025-07-25 18:51:34 +0300

    refactor: rename className field to grade in Class model

--- server/src/models/class.model.ts
+++ server/src/models/class.model.ts
@@ -1,11 +1,11 @@
-//src/models/class.model.ts
+//server/src/models/class.model.ts
 import { Schema, model, Types } from "mongoose";
 import { IClass } from "../types/school/school-core.types";
 
 const ClassSchema = new Schema<IClass>(
   {
     school: { type: Types.ObjectId, ref: "School", required: true },
-    className: { type: String, required: true },
+    grade: { type: String, required: true },
     stream: { type: String, required: true },
     academicYear: { type: String, required: true },
   },
