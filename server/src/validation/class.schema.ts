b7453bc0d4bd9282b7963615786b8da67e810bce
elainees-devs <elainees.devs@gmail.com>
2025-07-25 19:02:40 +0300

    refactor: update class schema to use 'grade' instead of 'className'

--- server/src/validation/class.schema.ts
+++ server/src/validation/class.schema.ts
@@ -1,10 +1,10 @@
-// src/validation/class.schema.ts
+// server/src/validation/class.schema.ts
 import { z } from "zod";
 import { objectId } from "./util";
 
 export const createClassSchema = z.object({
   school: objectId,
-  className: z.string().min(1),
+  grade: z.string().min(1),
   stream: z.string().min(3),
   academicYear: z.string().min(4),
 });
