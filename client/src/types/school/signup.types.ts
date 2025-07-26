639c8adb4408234634f28c41473d6279bb3e49a9
elainees-devs <elainees.devs@gmail.com>
2025-07-26 20:45:08 +0300

    refactor(types): update SignUpFormData – add required password, teacherId and classId fields; remove teachingSubjects

--- client/src/types/school/signup.types.ts
+++ client/src/types/school/signup.types.ts
@@ -2,26 +2,26 @@
 export type Role = 'school-admin' | 'teacher' | 'headteacher' | 'accountant';
 
 export interface SignUpFormData {
-  role?: Role;
-  school?: string;
-  firstName?: string;
-  middleName?: string;
-  lastName?: string;
-  email?: string;
-  secondaryEmail?: string;
-  primaryPhoneNumber?: string;
-  secondaryPhoneNumber?: string;
-  password?: string;
-  confirmPassword?: string;
-  nationality?: string;
-  avatarUrl?: string;
-  isTwoFactorEnabled?: boolean;
+  role?: Role
+  school?: string
+  firstName?: string
+  middleName?: string
+  lastName?: string
+  email?: string
+  secondaryEmail?: string
+  primaryPhoneNumber?: string
+  secondaryPhoneNumber?: string
+  password: string
+  confirmPassword?: string
+  nationality?: string
+  avatarUrl?: string
+  isTwoFactorEnabled?: boolean
+  teacherId: string
+  classId: string
+  isClassTeacher?:boolean
+  isActive: boolean
+  isLocked: boolean
 
-  // Role-specific
-  teacherId?: string;
-  isClassTeacher?: boolean;
-  class?: string;
-  teachingSubjects?: string[];
 }
 
 export interface SignUpContextType {
