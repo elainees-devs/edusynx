a6ba8083ae2bb31088d280436d84c1acdf3f537f
elainees-devs <elainees.devs@gmail.com>
2025-07-26 20:33:26 +0300

    style(signup): enhance PersonalInfoStep with dashed border, padding, and hover styles

--- client/src/components/signup/steps/personal-info.tsx
+++ client/src/components/signup/steps/personal-info.tsx
@@ -5,32 +5,40 @@ const PersonalInfoStep = ({ next }: { next: () => void }) => {
   const { formData, updateForm } = useSignUpContext();
 
   return (
-    <div className="space-y-4">
+    <div className="space-y-4 border-[1px] p-8 border-dashed border-gray-200">
       <input
-        className="w-full p-2 border"
+        className="w-full p-2 border rounded"
         placeholder="First Name"
-        value={formData.firstName || ''}
+        value={formData.firstName || ""}
         onChange={(e) => updateForm({ firstName: e.target.value })}
+        autoFocus
       />
       <input
-        className="w-full p-2 border"
+        className="w-full p-2 border rounded"
         placeholder="Middle Name"
-        value={formData.middleName || ''}
+        value={formData.middleName || ""}
         onChange={(e) => updateForm({ middleName: e.target.value })}
       />
       <input
-        className="w-full p-2 border"
+        className="w-full p-2 border rounded"
         placeholder="Last Name"
-        value={formData.lastName || ''}
+        value={formData.lastName || ""}
         onChange={(e) => updateForm({ lastName: e.target.value })}
       />
       <input
-        className="w-full p-2 border"
+        className="w-full p-2 border rounded"
         placeholder="Nationality"
-        value={formData.nationality || ''}
+        value={formData.nationality || ""}
         onChange={(e) => updateForm({ nationality: e.target.value })}
       />
-      <button onClick={next} className="px-4 py-2 mt-4 text-white bg-green-600 rounded">Next</button>
+      <div className="flex justify-end mt-4">
+        <button
+          onClick={next}
+          className="px-4 py-2 text-white bg-teal-400 hover:bg-teal-200 hover:text-gray rounded"
+        >
+          Next
+        </button>
+      </div>
     </div>
   );
 };
