2c90ef14c017ccdb7c5fcdbc28b9eb21d8629ee6
elainees-devs <elainees.devs@gmail.com>
2025-07-28 17:58:21 +0300

    feat(student): add familyNumber and studentPhotoUrl fields to IStudent interface

--- server/src/types/people/student.types.ts
+++ server/src/types/people/student.types.ts
@@ -3,20 +3,23 @@ import { Types } from "mongoose";
 import { IBaseUser } from "./user.types";
 import { StudentGender, StudentStatus } from "../enum/enum";
 import { BaseDocument } from "../common/base.types";
-import { IClass, ISchool } from "../school/school-core.types";
+import { IClass, ISchool, IStream } from "../school/school-core.types";
 
 export interface IStudent extends BaseDocument{
-  school: string | ISchool;
-  studentFirstName: string;
-  studentMiddleName: string;
-  studentLastName: string;
-  studentGender: StudentGender;
-  dateOfBirth: Date;
-  adm: number;
-  admissionDate: Date;
-  previousSchool?: string;
-  guardian: Types.ObjectId | IBaseUser;
-  classId: Types.ObjectId | IClass;
-  status: StudentStatus;
-  studentId?: string;
+  school: string | ISchool
+  studentFirstName: string
+  studentMiddleName: string
+  studentLastName: string
+  studentGender: StudentGender
+  dateOfBirth: Date
+  adm: number
+  admissionDate: Date
+  previousSchool?: string
+  guardian: Types.ObjectId | IBaseUser
+  classId: Types.ObjectId | IClass
+  stream: Types.ObjectId | IStream
+  status: StudentStatus
+  studentId?: string
+  familyNumber?: number
+  studentPhotoUrl?: string
 }
