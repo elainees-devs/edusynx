3bd934a139ce104549b2169cc31a4effbe319f85
elainees-devs <elainees.devs@gmail.com>
2025-07-29 10:28:05 +0300

    feat: Add school admin nav bar items

--- client/src/constants/sidebarMenu.ts
+++ client/src/constants/sidebarMenu.ts
@@ -12,7 +12,7 @@ import {
   FaClipboardList,
   FaStream,
 } from "react-icons/fa";
-import type { IconType } from "react-icons"; 
+import type { IconType } from "react-icons";
 import { teacherNavChildren } from "./sidebar-submenu";
 
 export interface NavItem {
@@ -32,11 +32,23 @@ export const superAdminNavItems: NavItem[] = [
 
 export const headTeacherNavItems: NavItem[] = [
   { name: "Dashboard", icon: FaTachometerAlt, path: "/dashboard" },
-  { name: "Teachers", icon: FaChalkboardTeacher, path: "/dashboard/teachers", children: teacherNavChildren },
+  {
+    name: "Teachers",
+    icon: FaChalkboardTeacher,
+    path: "/dashboard/teachers",
+    children: teacherNavChildren,
+  },
   { name: "Students", icon: FaUsers, path: "/dashboard/students" },
-   { name: "Streams", icon: FaStream, path: "/dashboard/streams" },
-
+  { name: "Streams", icon: FaStream, path: "/dashboard/streams" },
   { name: "Classes", icon: FaBook, path: "/head-teacher/class/new" },
   { name: "Reports", icon: FaClipboardList, path: "/dashboard/reports" },
+  { name: "Logout", icon: FaSignOutAlt, path: "/logout" },
+];
+
+export const schoolAdminNavItems: NavItem[] = [
+  { name: "Dashboard", icon: FaTachometerAlt, path: "/dashboard" },
+  { name: "Students", icon: FaSchool, path: "/dashboard/students" },
+  { name: "Reports", icon: FaUser, path: "/dashboard/reports" },
+  { name: "Settings", icon: FaCog, path: "/dashboard/settings" },
   { name: "Logout", icon: FaSignOutAlt, path: "/logout" },
 ];
