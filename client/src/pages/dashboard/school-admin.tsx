a69deb9f929ed04cc41fb501b2745ce6e8c1c31e
elainees-devs <elainees.devs@gmail.com>
2025-07-30 17:16:55 +0300

    feat: Integrate SchoolAdminDashboardOverview into school-admin dashboard layout

--- client/src/pages/dashboard/school-admin.tsx
+++ client/src/pages/dashboard/school-admin.tsx
@@ -1,14 +1,27 @@
-// src/pages/dashboard/school-admin.tsx
-import { Sidebar, Topbar } from "../../shared/layout/dashboard"
+// client/src/pages/dashboard/school-admin.tsx
+import { SchoolAdminDashboardOverview } from "../../components";
+import { Sidebar, Topbar } from "../../shared/layout/dashboard";
 
+const SchoolAdminDashBoard: React.FC = () => {
+  return (
+    <div className="h-screen flex flex-col">
+      {/* Topbar */}
+      <Topbar role="school-admin" />
 
-const SchoolAdminDashBoard: React.FC = () =>{
-    return(
-        <div>
-             <Topbar role="school-admin" />
-            <Sidebar role="school-admin" />
-            
+      {/* Main layout below topbar */}
+      <div className="flex flex-1 overflow-hidden">
+        {/* Sidebar */}
+        <div className="w-64 bg-white border-r h-full">
+          <Sidebar role="school-admin" />
         </div>
-    )
-}
-export default SchoolAdminDashBoard
+
+        {/* Main content */}
+        <main className="flex-1 overflow-y-auto p-6 bg-gray-50">
+          <SchoolAdminDashboardOverview />
+        </main>
+      </div>
+    </div>
+  );
+};
+
+export default SchoolAdminDashBoard;
