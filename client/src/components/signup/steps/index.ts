dabacad9da4d8e92f985e11bd6a33a1988138520
elainees-devs <elainees.devs@gmail.com>
2025-08-08 15:01:09 +0300

    chore(cleanup): remove unused components and pages, update form index

--- client/src/components/signup/steps/index.ts
+++
@@ -1,5 +0,0 @@
-// client/src/components/signup/steps/index.ts
-export { default as PersonalInfoStep } from './personal-info';
-export { default as ContactInfoStep } from './contact-info';
-export { default as AccountInfoStep } from './account-info';
-export { default as RoleSpecificStep } from './role-specific';
