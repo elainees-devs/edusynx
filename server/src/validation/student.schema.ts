1475f63f5ec548f6739f06426f0ab33389615a9f
elainees-devs <elainees.devs@gmail.com>
2025-08-08 14:52:55 +0300

    refactor(schema): update student schema to use new Gender enum and remove unused fields

--- server/src/validation/student.schema.ts
+++ server/src/validation/student.schema.ts
@@ -1,6 +1,6 @@
 // src/validation/student.schema.ts
 import { z } from "zod";
-import { StudentGender, StudentStatus } from "../types/enum/enum";
+import { Gender, StudentStatus } from "../types/enum/enum";
 import { objectId } from "./util";
 
 export const createStudentSchema = z.object({
@@ -8,12 +8,10 @@ export const createStudentSchema = z.object({
   studentFirstName: z.string().min(1),
   studentMiddleName: z.string().min(1),
   studentLastName: z.string().min(1),
-  studentGender: z.nativeEnum(StudentGender),
+  studentGender: z.nativeEnum(Gender),
   dateOfBirth: z.coerce.date(),
-  adm: z.number().int().min(1),
   admissionDate: z.coerce.date(),
   previousSchool: z.string().optional(),
-  guardian: objectId,
   classId: objectId,
   stream: objectId,
   status: z.nativeEnum(StudentStatus),
