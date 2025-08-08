b86c61b8400aee0dfab145b4298ba559873fae74
elainees-devs <elainees.devs@gmail.com>
2025-08-08 14:21:37 +0300

    chore(shared): export ImageDropzone and dashboard layout components

---
+++ client/src/shared/index.ts
@@ -0,0 +1,4 @@
+// client/src/shared/index.ts
+export { default as EduSynxLogo } from "./edusynx-logo";
+export { default as ImageDropzone } from "./image-dropozone"
+export { Sidebar, Topbar } from "./layout/dashboard";
