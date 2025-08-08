e81c44828d58c662802c20f3cc821ba25dd3ab3b
elainees-devs <elainees.devs@gmail.com>
2025-08-08 13:55:16 +0300

    chore(forms): export SignUpStepper and SchoolLogoDropzone components

--- client/src/components/forms/index.ts
+++ client/src/components/forms/index.ts
@@ -6,3 +6,5 @@ export {default as SuperAdminSignUpForm} from "./super-admin-signup-form";
 export {default as NewPasswordForm} from "./new-password-form";
 export {default as RegisterClassForm} from "./register-class-form";
 export {default as RegisterStreamForm} from "./register-stream-form";
+export { default as SignUpStepper } from "./signup/signup-stepper";
+export {default as SchoolLogoDropzone} from "./school/school-logo";
