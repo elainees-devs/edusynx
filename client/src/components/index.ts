c4eefdb059dcac561f9f3f281273043d9d0759b8
elainees-devs <elainees.devs@gmail.com>
2025-08-08 14:00:50 +0300

    refactor(components): simplify exports using wildcard re-exports and organize component structure

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -1,17 +1,12 @@
 // client/src/components/index.ts
-export { default as NavBar } from "./navbar";
-export { default as Hero } from "./hero";
-export { default as KeyFetures } from "./key-features";
-export { default as Benefits } from "./benefits";
-export { default as HowItWorks } from "./how-it-works";
-export {default as PricingSection} from "./pricing-section"
-export { default as LoginForm } from "./forms/login-form";
-export { default as ClassDetails } from "./data-table/class-list";
-export { default as ViewTeachersDetails } from "./teachers/view-teachers-details";
 
-// Dashboard Components
-export { default as SchoolAdminDashboardOverview } from "./dashboard/school-admin/dashboard-overview";
-
-// data table components
+export * from "./home";
+export * from "./forms";
 export * from "./data-table";
+export {default as LoginButton} from "./buttons/login-button"
+export * from "./dashboard"
+
+
+
+
 
