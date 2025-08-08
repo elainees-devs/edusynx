89c07a3aabae010ada1c2d692d28249b32b5ad53
elainees-devs <elainees.devs@gmail.com>
2025-08-08 14:34:13 +0300

    feat(hooks): add useClassOptions export to hooks index file

---
+++ client/src/hooks/index.ts
@@ -0,0 +1,6 @@
+// client/src/hooks/index.ts
+export { useGlobalState } from "./useGlobalContext";
+export { useClassOptions } from "./useClassOptions";
+export { useSchoolBySlug } from "./useSchoolBySlug";
+export { useStudentFormContext } from "./useStudentFormContext";
+export { useUserAuth } from "./useUserAuth";
