b8317b8f78575df08ce32de1495008102d62abfd
elainees-devs <elainees.devs@gmail.com>
2025-08-08 13:53:00 +0300

    chore(data-table): export SchoolList and TeacherList components

--- client/src/components/data-table/index.ts
+++ client/src/components/data-table/index.ts
@@ -1,5 +1,7 @@
 // client/src/components/data-table/index.tsx
 /* === This file aggregates exports from various data table components === */
 
-export {default as StudentList} from "./student-list";
-export {default as ClassList} from "./class-list";
+export {default as StudentList} from "./student-list"
+export {default as ClassList} from "./class-list"
+export {default as SchoolList} from "./school-list"
+export {default as TeacherList} from "./teacher-list"
