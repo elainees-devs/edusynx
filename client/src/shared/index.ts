d69897414db0b19cb150687fe8a1c69b4167d693
elainees-devs <elainees.devs@gmail.com>
2025-08-13 08:57:45 +0300

    chore(exports): re-export all from ./layout/ui for cleaner imports

--- client/src/shared/index.ts
+++ client/src/shared/index.ts
@@ -1,4 +1,5 @@
 // client/src/shared/index.ts
-export { default as EduSynxLogo } from "./edusynx-logo";
+export { default as EduSynxLogo } from "./edusynx-logo"
 export { default as ImageDropzone } from "./image-dropozone"
-export { Sidebar, Topbar } from "./layout/dashboard";
+export { Sidebar, Topbar } from "./layout/dashboard"
+export * from "./layout/ui"
