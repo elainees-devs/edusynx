9be2580360b367ff0530fe49201bbfae3303fd0e
elainees-devs <elainees.devs@gmail.com>
2025-08-16 17:28:44 +0300

    chore(api): consolidate and export all API modules from index.ts

---
+++ client/src/api/index.ts
@@ -0,0 +1,7 @@
+// client/src/api/index.ts
+export * from "./student.api";
+export * from "./guardian.api";
+export * from "./class.api";
+export * from "./school.api";
+export * from "./email.api";
+export * from "./auth.api";
