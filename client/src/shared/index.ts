ea92e6df0506378774e22035cf46944d63942291
elainees-devs <elainees.devs@gmail.com>
2025-08-16 17:19:57 +0300

    chore(exports): add Pagination component export

--- client/src/shared/index.ts
+++ client/src/shared/index.ts
@@ -2,4 +2,7 @@
 export { default as EduSynxLogo } from "./edusynx-logo"
 export { default as ImageDropzone } from "./image-dropozone"
 export { Sidebar, Topbar } from "./layout/dashboard"
+export { default as IconButton } from "./icon-button"
 export * from "./layout/ui"
+export { default as Pagination } from "./pagination";
+
