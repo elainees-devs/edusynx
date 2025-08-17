75ffa545f32f4c166f4799c32ccf04c4030e6ca6
elainees-devs <elainees.devs@gmail.com>
2025-08-17 16:38:41 +0300

    feat(components): export GuardianList from './guardian-list' in index

--- client/src/components/data-table/index.ts
+++ client/src/components/data-table/index.ts
@@ -2,6 +2,7 @@
 /* === This file aggregates exports from various data table components === */
 
 export {default as StudentList} from "./student-list"
+export {default as GuardianList} from "./guardian-list"
 export {default as ClassList} from "./class-list"
 export {default as SchoolList} from "./school-list"
 export {default as TeacherList} from "./teacher-list"
