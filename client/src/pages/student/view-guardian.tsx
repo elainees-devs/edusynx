8056588f07a0217b809956186204e513b98d6110
elainees-devs <elainees.devs@gmail.com>
2025-08-17 16:39:37 +0300

    feat(pages): enhance ViewGuardian with Sidebar, Topbar, and GuardianList layout

--- client/src/pages/student/view-guardian.tsx
+++ client/src/pages/student/view-guardian.tsx
@@ -1,5 +1,29 @@
 // client/src/page/student/view-guardian.tsx
+import { GuardianList } from "../../components";
+import { Sidebar, Topbar } from "../../shared";
+
+
 const ViewGuardian: React.FC = () => {
-  return <div><h1>View Guardian Page</h1></div>;
+  return (
+    <div className="flex h-screen">
+      {/* Sidebar */}
+      <div className="w-64 flex-shrink-0 bg-gray-100 overflow-y-auto">
+        <Sidebar role="headteacher"/>
+      </div>
+
+      {/* Main Content Area */}
+      <div className="flex flex-col flex-1 overflow-hidden">
+        {/* Topbar */}
+        <div className="flex-shrink-0">
+          <Topbar role="headteacher" />
+        </div>
+
+        {/* Student List */}
+        <div className="flex-1 overflow-auto p-4">
+          <GuardianList />
+      </div>
+    </div>
+    </div>
+  );
 };
 export default ViewGuardian;
