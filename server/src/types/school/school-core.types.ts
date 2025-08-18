260555c01c5974abb8805f4f68970c01e3211f7d
elainees-devs <elainees.devs@gmail.com>
2025-08-18 08:15:48 +0300

    chore: update IClass interface to include optional classTeacher and students fields

--- server/src/types/school/school-core.types.ts
+++ server/src/types/school/school-core.types.ts
@@ -1,6 +1,8 @@
 // server/src/types/school/school-core.types.ts
 import { Types } from "mongoose";
 import { BaseDocument } from "../common/base.types";
+import { ITeacher } from "../people/user.types";
+import { IStudent } from "../people/student.types";
 
 export interface ISchool extends BaseDocument {
   name: string
@@ -26,5 +28,7 @@ export interface IClass extends BaseDocument {
   grade: string
   stream: Types.ObjectId | IStream
   academicYear: string
+  classTeacher?: Types.ObjectId | ITeacher
+  students?: Array<Types.ObjectId | IStudent >
 }
 
