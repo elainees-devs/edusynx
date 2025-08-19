077211d130a405d4f20a84d21a4302f3e61a1765
elainees-devs <elainees.devs@gmail.com>
2025-08-19 14:19:51 +0300

    feat: create Mongoose model for ClassTeacher with references and totalStudents

---
+++ server/src/models/academics/class.teacher.model.ts
@@ -0,0 +1,17 @@
+// server/src/models/academics/class.teacher.model.ts
+import { Schema, Types, model } from "mongoose";
+import { IClassTeacher } from "../../types";
+
+const classTeacherSchema = new Schema<IClassTeacher>(
+  {
+    school: { type: Schema.Types.ObjectId, ref: "School", required: true },
+    teacher: { type: Schema.Types.ObjectId, ref: "Teacher", required: true },
+    grade: { type: Types.ObjectId, ref: "Class", required: true },
+    stream: { type: Types.ObjectId, ref: "Stream", required: true },
+    totalStudents: { type: Number, default: 0 },
+  },
+  { timestamps: true } 
+);
+
+export const ClassTeacher = model<IClassTeacher>("ClassTeacher", classTeacherSchema);
+
