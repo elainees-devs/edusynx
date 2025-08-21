0089e1eacb4944fcf3d49bb3177da3fdc3ea2e19
elainees-devs <elainees.devs@gmail.com>
2025-08-21 18:52:10 +0300

    chore(types): add IClassTeacher and ClassTeacher interfaces in academic.types

--- client/src/types/school/academic.types.ts
+++ client/src/types/school/academic.types.ts
@@ -1,7 +1,7 @@
 // server/src/types/school/academic.types.ts
 import type { BaseDocument } from "../common/base.types";
 import type { ITeacher } from "../people/user.types";
-import type { IClass, ISchool } from "./school-core.types";
+import type { IClass, ISchool, IStream } from "./school-core.types";
 
 
 export interface IDepartment{
@@ -16,4 +16,15 @@ export interface ISubject extends BaseDocument {
   departmentRef: string | IDepartment
   classRef: string | IClass
   teachers?: ITeacher[]
+}
+
+export interface IClassTeacher {
+  teacher: string | ITeacher
+  grade: string | IClass
+  stream: string | IStream
+  totalStudents: number
+}
+
+export interface ClassTeacher extends IClassTeacher{
+  _id: string
 }
