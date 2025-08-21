858fe541925bfb8a43f14ba5947fd72aa96f93db
elainees-devs <elainees.devs@gmail.com>
2025-08-21 18:44:10 +0300

    feat: export additional hooks (useClassTeachers, useStudents, useGuardians) from hooks index

--- client/src/hooks/index.ts
+++ client/src/hooks/index.ts
@@ -1,6 +1,10 @@
 // client/src/hooks/index.ts
 export { useGlobalState } from "./useGlobalContext";
 export { useClassOptions } from "./useClassOptions";
+export { useClassTeachers } from "./useClassTeacher";
 export { useSchoolBySlug } from "./useSchoolBySlug";
 export { useStudentFormContext } from "./useStudentFormContext";
 export { useUserAuth } from "./useUserAuth";
+export { useStudents } from "./useStudents";
+export { useGuardians } from "./useGuardians";
+
