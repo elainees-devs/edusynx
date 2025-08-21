66a2cff3935cdbe6f4a942b4f1b47b6ee8937fda
elainees-devs <elainees.devs@gmail.com>
2025-08-21 18:00:53 +0300

    feat(hooks): add useGuardians hook for fetching, searching, and sorting guardian data

---
+++ client/src/hooks/useGuardians.ts
@@ -0,0 +1,58 @@
+// client/src/hooks/useGuardian.ts
+// client/src/hooks/useGuardians.ts
+import { useEffect, useState } from "react";
+import type { Guardian } from "../types";
+import { getAllGuardians } from "../api";
+import { searchConfig } from "../constants";
+
+export const useGuardians = () => {
+  const [guardians, setGuardians] = useState<Guardian[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+  const [sortAsc, setSortAsc] = useState(true);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const { keys } = searchConfig.guardian;
+
+  const handleSort = () => setSortAsc((prev) => !prev);
+
+  const filteredGuardians = guardians
+    .filter((guardian) =>
+      keys.some((key) =>
+        String(guardian[key as keyof Guardian] ?? "")
+          .toLowerCase()
+          .includes(searchTerm.toLowerCase())
+      )
+    )
+    .sort((a, b) => {
+      const nameA = a.firstName.toLowerCase();
+      const nameB = b.firstName.toLowerCase();
+      return sortAsc ? nameA.localeCompare(nameB) : nameB.localeCompare(nameA);
+    });
+
+  useEffect(() => {
+    const fetchGuardians = async () => {
+      try {
+        const data = await getAllGuardians();
+        setGuardians(data);
+      } catch (err) {
+        setError("Failed to fetch guardians");
+        console.error(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchGuardians();
+  }, []);
+
+  return {
+    guardians: filteredGuardians,
+    loading,
+    error,
+    sortAsc,
+    searchTerm,
+    setSearchTerm,
+    handleSort,
+  };
+};
