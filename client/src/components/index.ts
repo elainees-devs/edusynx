c0fe715ae2f886f25f3cab810f75cb6ce836a00e
elainees-devs <elainees.devs@gmail.com>
2025-08-21 18:39:11 +0300

    refactor: update components index to export data-list instead of data-table

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -2,7 +2,6 @@
 
 export * from "./home";
 export * from "./forms";
-// export * from "./data-table";
 export * from "./data-list";
 export {default as LoginButton} from "./buttons/login-button"
 export * from "./dashboard"
