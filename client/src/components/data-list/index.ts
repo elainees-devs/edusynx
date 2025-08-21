981dc8b3242604608338ecfef551f7f20ecb243b
elainees-devs <elainees.devs@gmail.com>
2025-08-21 18:23:17 +0300

    feat: export data-list components (SchoolList, ClassList, ClassOverviewList, StudentList, GuardianList, TeacherList)

---
+++ client/src/components/data-list/index.ts
@@ -0,0 +1,7 @@
+// client/src/components/data-list/index.ts
+export {default as SchoolList} from './school-list'
+export {default as ClassList} from './class-list'
+export {default as ClassOverviewList} from './class-overview-list.tsx'
+export {default as StudentList} from './studet-list.tsx'
+export {default as GuardianList} from './guardian-list.tsx'
+export {default as TeacherList} from './teacher-list.tsx'
