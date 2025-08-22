849bcfb12157cc6534914de74ee29c3eecc0c748
elainees-devs <elainees.devs@gmail.com>
2025-08-22 11:26:31 +0300

    fix(class-teacher-model): update teacher ref from 'Teacher' to 'User'

--- server/src/models/academics/class.teacher.model.ts
+++ server/src/models/academics/class.teacher.model.ts
@@ -5,7 +5,7 @@ import { IClassTeacher } from "../../types";
 const classTeacherSchema = new Schema<IClassTeacher>(
   {
     school: { type: Schema.Types.ObjectId, ref: "School", required: true },
-    teacher: { type: Schema.Types.ObjectId, ref: "Teacher", required: true },
+    teacher: { type: Schema.Types.ObjectId, ref: "User", required: true },
     grade: { type: Types.ObjectId, ref: "Class", required: true },
     stream: { type: Types.ObjectId, ref: "Stream", required: true },
     totalStudents: { type: Number, default: 0 },
