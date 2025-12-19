12fa462046615e42cc7da5decae8794833f26276
elainees-devs <elainees.devs@gmail.com>
2025-12-19 10:49:34 +0300

    feat(dashboard): add export for AccountantDashboardOverview

--- client/src/components/dashboard/index.ts
+++ client/src/components/dashboard/index.ts
@@ -2,3 +2,4 @@
 export { default as PrincipalDashboardOverview } from "./principal/dashboard-overview";    
 export { default as SchoolAdminDashboardOverview } from "./school-admin/dashboard-overview";
 export { default as SuperAdminDashboardOverview } from "./super-admin/dashboard-overview";
+export { default as AccountantDashboardOverview } from "./accountant/dashboard-overview";
