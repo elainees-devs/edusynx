71a5845c1cc2ad1fa1c174bbc26cee1e574b5164
elainees-devs <elainees.devs@gmail.com>
2026-01-26 19:29:41 +0300

    refactor(types): generalize pagination types with reusable PaginatedResponse

--- client/src/types/pagination/pagination.types.ts
+++ client/src/types/pagination/pagination.types.ts
@@ -1,9 +1,29 @@
+// client/src/types/pagination/pagination.types.ts
 import type { Student } from "../people/student.types";
+import type { ITeacher } from "../people/user.types";
+import type { IClass } from "../school/school-core.types";
 
-export interface PaginatedStudents {
-  data: Student[];
+export interface GetPageParams{
+  page: number
+  limit: number
+  search?: string
+  sort?: "asc"|"desc"
+
+}
+
+export interface PaginatedResponse<T> {
+  data: T[];
   page: number;
   limit: number;
   total: number;
   totalPages: number;
 }
+
+export type PaginatedStudents = PaginatedResponse<Student>;
+export type PaginatedTeachers = PaginatedResponse<ITeacher>;
+export type PaginatedClasses = PaginatedResponse<IClass>;
+
+
+
+
+
