8d74fc7547d01688c0e614fae286bb4f97e2f4ec
elainees-devs <elainees.devs@gmail.com>
2026-01-28 17:18:22 +0300

    chore(validation): export guardian validation schemas

---
+++ server/src/validation/index.ts
@@ -0,0 +1,13 @@
+// server/src/validation/index.ts
+export { createGuardianSchema, updateGuardianSchema } from './guardian.schema';
+export { createClassSchema, updateClassSchema } from './class.schema';
+export { createStudentSchema, updateStudentSchema } from './student.schema';    
+export { createUserSchema, updateUserSchema } from './user.schema';
+export { createSchoolSchema, updateSchoolSchema } from './school.schema';
+export { createSubjectSchema, updateSubjectSchema } from './subject.schema';
+export { createExamSchema, updateExamSchema } from './exam.schema';
+export { createEventSchema, updateEventSchema } from './event.schema';
+export { createFeeSchema, updateFeeSchema } from './fee.schema';
+export { createInvoiceSchema, updateInvoiceSchema } from './invoice.schema';
+export { createNotificationSchema, updateNotificationSchema } from './notification.schema';
+export { loginSchema } from './login.schema';
