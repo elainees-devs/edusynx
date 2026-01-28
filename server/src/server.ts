cbcc461027eb809171005f745c05fda65403c936
elainees-devs <elainees.devs@gmail.com>
2026-01-28 18:12:47 +0300

    chore: update server.ts with dotenv path, extended CORS origins, and type reference

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,8 @@
 // server/src/server.ts
 /// <reference types="./types/express" />
+import dotenv from "dotenv";
+import path from "path";
 import express, { Request, Response } from "express";
-import { configDotenv } from "dotenv";
 import cors from "cors";
 import helmet from "helmet";
 import connectDB from "./config/db";
@@ -37,25 +38,31 @@ import {
 } from "./routes";
 import { SchoolController } from "./controllers";
 import adminRouter from "./routes/super-admin.route";
-
-configDotenv();
+dotenv.config({
+  path: path.resolve(__dirname, "../.env"),
+});
 
 const app = express();
 const apiRouter = express.Router();
 const PORT: number = parseInt(process.env.PORT || "5000", 10);
 const FRONTEND_BASE_URL =
   process.env.FRONTEND_BASE_URL || "http://localhost:5173";
-
+  
 
 // Middleware
 app.use(express.json());
 app.use(
   cors({
-    origin: [FRONTEND_BASE_URL], // allow your frontend
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    credentials: true, // if you need cookies/auth
+    origin: [
+      FRONTEND_BASE_URL,        // Vite frontend
+      "http://localhost:5173",  // Swagger UI
+    ],
+    methods: ["GET", "POST", "PUT","PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
   })
 );
+
 
 app.use(helmet());
 
