8ae76c7966803b3c8adb8939950c12160b479f05
elainees-devs <elainees.devs@gmail.com>
2026-01-29 17:13:29 +0300

    refactor(dashboard): replace HEADTEACHER role with PRINCIPAL in user dashboard routes

--- client/src/constants/user-dashboard.ts
+++ client/src/constants/user-dashboard.ts
@@ -2,7 +2,7 @@
 export const userDashboard = [
   { role: "ADMIN", path: "/dashboard/super-admin" },
   { role: "SCHOOL-ADMIN", path: "/dashboard/school-admin" },
-  { role: "HEADTEACHER", path: "/dashboard/headteacher" },
+  { role: "PRINCIPAL", path: "/dashboard/principal" },
   { role: "GUARDIAN", path: "/dashboard/guardian" },
   { role: "ACCOUNTANT", path: "/dashboard/accountant" },
 ];
