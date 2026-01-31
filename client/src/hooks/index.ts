cc09fd89b2786af66543617cde592586da369a4c
elainees-devs <elainees.devs@gmail.com>
2026-01-31 10:01:47 +0300

    chore(hooks): remove useStudents and useGuardians from hooks index exports

--- client/src/hooks/index.ts
+++ client/src/hooks/index.ts
@@ -5,6 +5,5 @@ export { useClassTeachers } from "./useClassTeacher";
 export { useSchoolBySlug } from "./useSchoolBySlug";
 export { useStudentFormContext } from "./useStudentFormContext";
 export { useUserAuth } from "./useUserAuth";
-export { useStudents } from "./useStudents";
-export { useGuardians } from "./useGuardians";
+
 
