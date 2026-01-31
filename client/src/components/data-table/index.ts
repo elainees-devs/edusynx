f9a508fa4ce2eb64e0a1aed9bee56ba1cc1c490d
elainees-devs <elainees.devs@gmail.com>
2026-01-31 09:50:55 +0300

    feat(data-table): add StreamTable export to data-table index

--- client/src/components/data-table/index.ts
+++ client/src/components/data-table/index.ts
@@ -3,6 +3,7 @@
 
 export {default as ClassOverviewTable} from'./class-overview-table.tsx'
 export {default as ClassTable} from'./class-table.tsx'
+export {default as StreamTable} from'./streams-table.tsx'
 export {default as TeacherTable} from'./teacher-table.tsx'
 export {default as GuardianTable} from'./guardian-table.tsx'
 export {default as StudentTable} from'./student-table.tsx'
