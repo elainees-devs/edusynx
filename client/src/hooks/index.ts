816e542cd867064bfc4d8a426b955a889a6cd32e
elainees-devs <elainees.devs@gmail.com>
2026-01-31 18:19:01 +0300

    chore: export ClassOption type from hooks index for reuse

--- client/src/hooks/index.ts
+++ client/src/hooks/index.ts
@@ -1,9 +1,10 @@
 // client/src/hooks/index.ts
 export { useGlobalState } from "./useGlobalContext";
-export { useClassOptions } from "./useClassOptions";
+export { useClassOptions, type ClassOption } from "./useClassOptions";
 export { useClassTeachers } from "./useClassTeacher";
 export { useSchoolBySlug } from "./useSchoolBySlug";
 export { useStudentFormContext } from "./useStudentFormContext";
 export { useUserAuth } from "./useUserAuth";
+
 
 
