60a779f6768e8c064c03b3abc469cfb11e2d7a8e
elainees-devs <elainees.devs@gmail.com>
2026-01-31 09:52:16 +0300

    feat(data-list): add StreamList export to data-list index

--- client/src/components/data-list/index.ts
+++ client/src/components/data-list/index.ts
@@ -1,6 +1,7 @@
 // client/src/components/data-list/index.ts
 export {default as SchoolList} from './school-list'
 export {default as ClassList} from './class-list'
+export {default as StreamList} from './stream-list.tsx'
 export {default as ClassOverviewList} from './class-overview-list.tsx'
 export {default as StudentList} from './student-list.tsx'
 export {default as GuardianList} from './guardian-list.tsx'
