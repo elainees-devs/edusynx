5d7406944a9e7f0315e9a90e352aa8b3de8dbcd7
elainees-devs <elainees.devs@gmail.com>
2026-02-02 11:46:02 +0300

    chore(dto): re-export subscription DT

--- server/src/dto/index.ts
+++ server/src/dto/index.ts
@@ -1,5 +1,8 @@
 // server/src/dto/index.ts
+import e from 'express'
+
 export * from './school-structure.dto'
 export * from './user.dto'
 export * from './finance.dto'
 export * from './misc.dto'
+export * from './subscription.dto'
