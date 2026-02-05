964b71a5e04f4ea4335367351485840f3a0b83d2
elainees-devs <elainees.devs@gmail.com>
2026-02-05 11:50:09 +0300

    chore: export base-user.api and attendance.api in api index

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -1,6 +1,7 @@
 // client/src/api/index.ts
 export * from "./student.api";
 export * from "./guardian.api";
+export * from "./base-user.api"
 export * from "./class.api";
 export * from "./stream.api"
 export * from "./subject.api";
@@ -8,5 +9,6 @@ export * from "./school.api";
 export * from "./email.api";
 export * from "./auth.api";
 export * from "./class.teacher.api";
+export * from "./attendance.api";
 
 export {loginSuperAdmin, sendPasswordResetEmail} from "./auth/super-admin-auth";
