c9668e3cc5a8b0e2a9d98eda6d7c1811537ae107
elainees-devs <elainees.devs@gmail.com>
2026-02-05 11:53:17 +0300

    feat: add RegisterAttendancePage wrapping AttendanceForm component

---
+++ client/src/pages/attendance/register-attendance.tsx
@@ -0,0 +1,13 @@
+// client/src/pages/attendance/register-attendance.tsx
+import React from "react";
+import AttendanceForm from "../../components/forms/attendance-form";
+
+const RegisterAttendancePage: React.FC = () => {
+  return (
+    <div className="mt-8">
+      <AttendanceForm />
+    </div>
+  );
+};
+
+export default RegisterAttendancePage;
