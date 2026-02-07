a797693a536b311f96b651109506b91a4196f54e
elainees-devs <elainees.devs@gmail.com>
2026-02-07 09:39:09 +0300

    feat: add ViewAttendance component

---
+++ client/src/pages/attendance/view-attendance.tsx
@@ -0,0 +1,9 @@
+const ViewAttendance: React.FC = () => {
+  return (
+    <div>
+      <h1>View Attendance</h1>
+      {/* Add your attendance viewing logic here */}
+    </div>
+  );
+}
+export default ViewAttendance;
