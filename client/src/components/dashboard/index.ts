441fa38db1146bfb48ef351124645dc6b26ea6c8
elainees-devs <elainees.devs@gmail.com>
2026-03-08 19:35:55 +0300

    feat(dashboard): add Teacher and Guardian dashboard overview exports

--- client/src/components/dashboard/index.ts
+++ client/src/components/dashboard/index.ts
@@ -3,3 +3,6 @@ export { default as PrincipalDashboardOverview } from "./principal/dashboard-ove
 export { default as SchoolAdminDashboardOverview } from "./school-admin/dashboard-overview";
 export { default as SuperAdminDashboardOverview } from "./super-admin/dashboard-overview";
 export { default as AccountantDashboardOverview } from "./accountant/dashboard-overview";
+export { default as TeacherDashboardOverview } from "./teacher/dashboard-overview";
+export {default as GuardianDashboardOverview } from "./guardian/dashboar-overview";
+
