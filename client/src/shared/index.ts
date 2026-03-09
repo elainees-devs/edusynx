e45f5aabb94fd01563b1e90a35abf65828011630
elainees-devs <elainees.devs@gmail.com>
2026-03-09 13:58:25 +0300

    refactor(shared): update exports to PascalCase filenames in index.ts

--- client/src/shared/index.ts
+++ client/src/shared/index.ts
@@ -1,8 +1,8 @@
 // client/src/shared/index.ts
-export { default as EduSynxLogo } from "./edusynx-logo"
-export { default as ImageDropzone } from "./image-dropozone"
-export { Sidebar, Topbar } from "./layout/dashboard"
-export { default as IconButton } from "./icon-button"
-export * from "./layout/ui"
-export { default as Pagination } from "./pagination";
+export { default as EdusynxLogo } from "./EdusynxLogo";
+export { default as ImageDropzone } from "./ImageDropzone";
+export { Sidebar, Topbar } from "./layout/dashboard";
+export { default as IconButton } from "./IconButton";
+export * from "./layout/ui";
+export { default as Pagination } from "./Pagination";
 
