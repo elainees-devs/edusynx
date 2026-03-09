09a68c850a8dce70078505c6b47dc361c567c9fe
elainees-devs <elainees.devs@gmail.com>
2026-03-09 14:55:07 +0300

    fix: correct import path for IBaseUser from UserTypes

--- client/src/types/school/Allocation.ts
+++ client/src/types/school/Allocation.ts
@@ -1,22 +1,24 @@
-// client/src/types/school/allocation.ts
-import type { IBaseUser } from "../people/user.types";
+// client/src/types/school/Allocation.ts
 
+import type { IBaseUser } from "../people/UserTypes";
 
 export type Teacher = Pick<
   IBaseUser,
-  "firstName"|"middleName"| "lastName"|"primaryPhoneNumber" | "secondaryPhoneNumber" | "email"
+  | "firstName"
+  | "middleName"
+  | "lastName"
+  | "primaryPhoneNumber"
+  | "secondaryPhoneNumber"
+  | "email"
 > & {
-  _id: string
-  isActive: boolean
-  employmentNo: string
+  _id: string;
+  isActive: boolean;
+  employmentNo: string;
 };
-
 
 export type ClassAllocation = {
-  id: number
-  className: string
-  stream: string
-  teacher: Teacher
+  id: number;
+  className: string;
+  stream: string;
+  teacher: Teacher;
 };
-
-
