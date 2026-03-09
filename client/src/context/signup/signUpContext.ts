5b1c203e63e9f6d2f5c820da19419dd1f2d0b412
elainees-devs <elainees.devs@gmail.com>
2026-03-09 16:45:21 +0300

    refactor: update import to SignUpTypes from signup.types

--- client/src/context/signup/signUpContext.ts
+++ client/src/context/signup/signUpContext.ts
@@ -1,6 +1,7 @@
 // client/src/context/signup/signUpContext.ts
-import { createContext } from 'react';
-import type { SignUpContextType } from '../../types/school/signup.types';
+import { createContext } from "react";
+import type { SignUpContextType } from "../../types/school/SignupTypes";
 
-
-export const SignUpContext = createContext<SignUpContextType | undefined>(undefined);
+export const SignUpContext = createContext<SignUpContextType | undefined>(
+  undefined,
+);
