b0f180273e7157f972004d16ffef86f895c0fe47
elainees-devs <elainees.devs@gmail.com>
2026-03-30 17:30:36 +0300

    feat(validation): export AssessmentTemplate schemas in cbc.validation.schema

--- server/src/validation/index.ts
+++ server/src/validation/index.ts
@@ -1,30 +1,46 @@
 // server/src/validation/index.ts
-export { createGuardianSchema, updateGuardianSchema } from './guardian.schema';
-export { createClassSchema, updateClassSchema } from './class.schema';
-export { createStudentSchema, updateStudentSchema } from './student.schema';    
-export { createUserSchema, updateUserSchema } from './user.schema';
-export { createSchoolSchema, updateSchoolSchema } from './school.schema';
-export { createSubjectSchema, updateSubjectSchema } from './subject.schema';
-export { createTeacherSubjectSchema, updateTeacherSubjectSchema } from './teacher-subject.schema';
-export { createExamSchema, updateExamSchema } from './exam.schema';
-export { createEventSchema, updateEventSchema } from './event.schema';
-export { createFeeSchema, updateFeeSchema } from './fee.schema';
-export { createInvoiceSchema, updateInvoiceSchema } from './invoice.schema';
-export { createNotificationSchema, updateNotificationSchema } from './notification.schema';
-export { loginSchema } from './login.schema';
-export { createSubscriptionSchema, updateSubscriptionSchema } from './subscription.schema';
-export { createSubscriptionPlanSchema, updateSubscriptionPlanSchema } from './subscription-plan.schema';
-export { createAttendanceSchema, updateAttendanceSchema } from './attendance.schema';
+export { createGuardianSchema, updateGuardianSchema } from "./guardian.schema";
+export { createClassSchema, updateClassSchema } from "./class.schema";
+export { createStudentSchema, updateStudentSchema } from "./student.schema";
+export { createUserSchema, updateUserSchema } from "./user.schema";
+export { createSchoolSchema, updateSchoolSchema } from "./school.schema";
+export { createSubjectSchema, updateSubjectSchema } from "./subject.schema";
 export {
-	createAssessmentSchema,
-	updateAssessmentSchema,
-	createLearningOutcomeSchema,
-	updateLearningOutcomeSchema,
-	createSubStrandSchema,
-	updateSubStrandSchema,
-	createStrandSchema,
-	updateStrandSchema,
-	createCompetencySchema,
-	updateCompetencySchema,
-} from './cbc.validation.schema';
-
+  createTeacherSubjectSchema,
+  updateTeacherSubjectSchema,
+} from "./teacher-subject.schema";
+export { createExamSchema, updateExamSchema } from "./exam.schema";
+export { createEventSchema, updateEventSchema } from "./event.schema";
+export { createFeeSchema, updateFeeSchema } from "./fee.schema";
+export { createInvoiceSchema, updateInvoiceSchema } from "./invoice.schema";
+export {
+  createNotificationSchema,
+  updateNotificationSchema,
+} from "./notification.schema";
+export { loginSchema } from "./login.schema";
+export {
+  createSubscriptionSchema,
+  updateSubscriptionSchema,
+} from "./subscription.schema";
+export {
+  createSubscriptionPlanSchema,
+  updateSubscriptionPlanSchema,
+} from "./subscription-plan.schema";
+export {
+  createAttendanceSchema,
+  updateAttendanceSchema,
+} from "./attendance.schema";
+export {
+  createAssessmentSchema,
+  updateAssessmentSchema,
+  createLearningOutcomeSchema,
+  updateLearningOutcomeSchema,
+  createSubStrandSchema,
+  updateSubStrandSchema,
+  createStrandSchema,
+  updateStrandSchema,
+  createCompetencySchema,
+  updateCompetencySchema,
+  createAssessmentTemplateSchema,
+  updateAssessmentTemplateSchema,
+} from "./cbc.validation.schema";
