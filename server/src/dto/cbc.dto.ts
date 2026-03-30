4665869c93ab013a74ef2a0c2a74b47a5f72c81c
elainees-devs <elainees.devs@gmail.com>
2026-03-30 16:15:27 +0300

    chore(types): add DTOs for creating CBC entities without metadata fields

---
+++ server/src/dto/cbc.dto.ts
@@ -0,0 +1,7 @@
+import { IAssessment, ILearningOutcome, ISubStrand, IStrand, ICompetency } from "../types/cbc.types";
+
+export type CreateAssessmentDTO = Omit<IAssessment, "_id" | "createdAt" | "updatedAt" | "created_at" | "updated_at">;
+export type CreateLearningOutcomeDTO = Omit<ILearningOutcome, "_id" | "createdAt" | "updatedAt" | "created_at" | "updated_at">;
+export type CreateSubStrandDTO = Omit<ISubStrand, "_id" | "createdAt" | "updatedAt" | "created_at" | "updated_at">;
+export type CreateStrandDTO = Omit<IStrand, "_id" | "createdAt" | "updatedAt" | "created_at" | "updated_at">;
+export type CreateCompetencyDTO = Omit<ICompetency, "_id" | "createdAt" | "updatedAt" | "created_at" | "updated_at">;
