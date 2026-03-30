397593add21fd01626e3ebd198503e6bbe3ef922
elainees-devs <elainees.devs@gmail.com>
2026-03-30 20:55:13 +0300

    feat(cbc): add RegisterStrand component with placeholder form

---
+++ client/src/pages/cbc/RegisterStrand.tsx
@@ -0,0 +1,11 @@
+
+const RegisterStrand: React.FC = () => {
+  return (
+    <div>
+      <h1>Register Strand</h1>
+      {/* Form to register a new strand */}
+    </div>
+  );
+};
+
+export default RegisterStrand;
