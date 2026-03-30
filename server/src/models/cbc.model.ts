9575df52e5d9be7ce9cf1526dc6fd94d4aaf1250
elainees-devs <elainees.devs@gmail.com>
2026-03-30 15:49:54 +0300

    refactor(models): remove nested strands and substrands from CBC schemas

--- server/src/models/cbc.model.ts
+++ server/src/models/cbc.model.ts
@@ -1,32 +1,24 @@
-import mongoose, { Schema, Document, Types } from 'mongoose';
-import { IAssessment } from '../types';
-
+import mongoose, { Schema, Document, Types } from "mongoose";
+import {
+  IAssessment,
+  ICompetency,
+  ILearningOutcome,
+  IStrand,
+  ISubStrand,
+} from "../types";
 
 const AssessmentSchema = new Schema<IAssessment>({
   title: { type: String, required: true },
   description: String,
-  type: { type: String, enum: ['formative', 'summative'], required: true },
+  type: { type: String, enum: ["formative", "summative"], required: true },
   criteria: [String],
 });
-
-export interface ILearningOutcome extends Document {
-  code: string;
-  description: string;
-  assessments: Types.DocumentArray<IAssessment>;
-}
 
 const LearningOutcomeSchema = new Schema<ILearningOutcome>({
   code: { type: String, required: true },
   description: { type: String, required: true },
   assessments: [AssessmentSchema],
 });
-
-export interface ISubStrand extends Document {
-  code: string;
-  title: string;
-  description?: string;
-  learningOutcomes: Types.DocumentArray<ILearningOutcome>;
-}
 
 const SubStrandSchema = new Schema<ISubStrand>({
   code: { type: String, required: true },
@@ -35,26 +27,11 @@ const SubStrandSchema = new Schema<ISubStrand>({
   learningOutcomes: [LearningOutcomeSchema],
 });
 
-export interface IStrand extends Document {
-  code: string;
-  title: string;
-  description?: string;
-  subStrands: Types.DocumentArray<ISubStrand>;
-}
-
 const StrandSchema = new Schema<IStrand>({
   code: { type: String, required: true },
   title: { type: String, required: true },
   description: String,
-  subStrands: [SubStrandSchema],
 });
-
-export interface ICompetency extends Document {
-  code: string;
-  title: string;
-  description?: string;
-  strands: Types.DocumentArray<IStrand>;
-}
 
 const CompetencySchema = new Schema<ICompetency>({
   code: { type: String, required: true },
@@ -62,8 +39,20 @@ const CompetencySchema = new Schema<ICompetency>({
   description: String,
 });
 
-export const AssessmentModel = mongoose.model<IAssessment>('Assessment', AssessmentSchema);
-export const LearningOutcomeModel = mongoose.model<ILearningOutcome>('LearningOutcome', LearningOutcomeSchema);
-export const SubStrandModel = mongoose.model<ISubStrand>('SubStrand', SubStrandSchema);
-export const StrandModel = mongoose.model<IStrand>('Strand', StrandSchema);
-export const CompetencyModel = mongoose.model<ICompetency>('Competency', CompetencySchema);
+export const AssessmentModel = mongoose.model<IAssessment>(
+  "Assessment",
+  AssessmentSchema,
+);
+export const LearningOutcomeModel = mongoose.model<ILearningOutcome>(
+  "LearningOutcome",
+  LearningOutcomeSchema,
+);
+export const SubStrandModel = mongoose.model<ISubStrand>(
+  "SubStrand",
+  SubStrandSchema,
+);
+export const StrandModel = mongoose.model<IStrand>("Strand", StrandSchema);
+export const CompetencyModel = mongoose.model<ICompetency>(
+  "Competency",
+  CompetencySchema,
+);
