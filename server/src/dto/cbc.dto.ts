037282cfbbc609866b162658634f56944e206f4f
elainees-devs <elainees.devs@gmail.com>
2026-03-30 16:55:51 +0300

    feat(dto): add CreateAssessmentTemplateDTO type based on IAssessmentTemplate

--- server/src/dto/cbc.dto.ts
+++ server/src/dto/cbc.dto.ts
@@ -1,10 +1,10 @@
-import { IAssessment, ILearningOutcome, ISubStrand, IStrand, ICompetency } from "../types/cbc.types";
+import { IAssessment, ILearningOutcome, ISubStrand, IStrand, ICompetency, IAssessmentTemplate } from "../types/cbc.types";
 
 export type CreateAssessmentDTO = Omit<IAssessment, "_id" | "createdAt" | "updatedAt" | "created_at" | "updated_at">;
 export type CreateLearningOutcomeDTO = Omit<ILearningOutcome, "_id" | "createdAt" | "updatedAt" | "created_at" | "updated_at">;
 export type CreateSubStrandDTO = Omit<ISubStrand, "_id" | "createdAt" | "updatedAt" | "created_at" | "updated_at">;
 export type CreateStrandDTO = Omit<IStrand, "_id" | "createdAt" | "updatedAt" | "created_at" | "updated_at">;
 export type CreateCompetencyDTO = Omit<ICompetency, "_id" | "createdAt" | "updatedAt" | "created_at" | "updated_at">;
-export type CreateAssessmentTemplateDTO = Omit<IAssessment, "_id" | "createdAt" | "updatedAt" | "created_at" | "updated_at">;
+export type CreateAssessmentTemplateDTO = Omit<IAssessmentTemplate, "_id" | "createdAt" | "updatedAt" | "created_at" | "updated_at">;
 
 
