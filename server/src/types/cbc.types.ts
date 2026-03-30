39f68159f165a5a95ebca602848a432d1f967dac
elainees-devs <elainees.devs@gmail.com>
2026-03-30 13:28:07 +0300

    feat: add CBC TypeScript interfaces for competencies, strands, sub-strands, learning outcomes, and assessments

---
+++ server/src/types/cbc.types.ts
@@ -0,0 +1,40 @@
+// CBC Types for EduSynx
+
+export interface IAssessment {
+  _id?: string;
+  title: string;
+  description?: string;
+  type: 'formative' | 'summative';
+  criteria?: string[];
+}
+
+export interface ILearningOutcome {
+  _id?: string;
+  code: string;
+  description: string;
+  assessments?: IAssessment[];
+}
+
+export interface ISubStrand {
+  _id?: string;
+  code: string;
+  title: string;
+  description?: string;
+  learningOutcomes?: ILearningOutcome[];
+}
+
+export interface IStrand {
+  _id?: string;
+  code: string;
+  title: string;
+  description?: string;
+  subStrands?: ISubStrand[];
+}
+
+export interface ICompetency {
+  _id?: string;
+  code: string;
+  title: string;
+  description?: string;
+  strands?: IStrand[];
+}
