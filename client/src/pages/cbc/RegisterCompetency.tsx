9a014cfdacb607577d05a388dddec3a06bbf53cc
elainees-devs <elainees.devs@gmail.com>
2026-03-30 19:32:35 +0300

    feat(ui): add RegisterCompetency page component

---
+++ client/src/pages/cbc/RegisterCompetency.tsx
@@ -0,0 +1,11 @@
+
+const RegisterCompetency: React.FC = () => {
+  return (
+    <div>
+      <h1>Register Competency</h1>
+      {/* Form for registering competency goes here */}
+    </div>
+  );
+};
+
+export default RegisterCompetency;
