158247fd0eda22479fabc318a176ba7c65246710
elainees-devs <elainees.devs@gmail.com>
2026-04-03 12:57:40 +0300

    feat(tables): export CompetencyTable and StrandTable components

--- client/src/components/data-table/index.ts
+++ client/src/components/data-table/index.ts
@@ -1,6 +1,7 @@
 // client/src/components/data-table/index.tsx
 /* === This file aggregates exports from various data table components === */
 
+
 export { default as ClassOverviewTable } from './ClassOverviewTable';
 export { default as ClassTable } from './ClassTable';
 export { default as StreamsTable } from './StreamsTable';
@@ -9,5 +10,6 @@ export { default as TeacherTable } from './TeacherTable';
 export { default as GuardianTable } from './GuardianTable';
 export { default as StudentTable } from './StudentTable';
 export { default as AttendanceTable } from './AttendanceTable';
-
+export { default as CompetencyTable } from './CompetencyTable';
+export { default as StrandTable } from './StrandTable';
 
