8b2c1c49424eb70a0a6c3cce6e3da84ec194bd7a
elainees-devs <elainees.devs@gmail.com>
2026-04-03 13:03:11 +0300

    feat(views): add ViewCompetency page with Sidebar, Topbar, and CompetencyList

---
+++ client/src/pages/cbc/ViewCompetency.tsx
@@ -0,0 +1,29 @@
+import { CompetencyList } from "../../components";
+import { Sidebar, Topbar } from "../../shared";
+
+
+const ViewCompetency: React.FC = () => {
+	return (
+		<div className="flex h-screen">
+			{/* Sidebar */}
+			<div className="w-64 flex-shrink-0 bg-gray-100 overflow-y-auto">
+				<Sidebar role={["principal"]} />
+			</div>
+
+			{/* Main Content Area */}
+			<div className="flex flex-col flex-1 overflow-hidden">
+				{/* Topbar */}
+				<div className="flex-shrink-0">
+					<Topbar role={["principal"]} />
+				</div>
+
+				{/* Competency List */}
+				<div className="flex-1 overflow-auto p-4">
+					<CompetencyList />
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default ViewCompetency;
