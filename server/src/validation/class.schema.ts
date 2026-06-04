68e11dfaba40239ee2ab36048856f5f945afe33d
elainees-devs <elainees.devs@gmail.com>
2026-06-04 16:54:24 +0300

    fix: remove school property from class validation

--- server/src/validation/class.schema.ts
+++ server/src/validation/class.schema.ts
@@ -3,7 +3,6 @@ import { z } from "zod";
 import { objectId } from "./util";
 
 export const createClassSchema = z.object({
-  school: objectId,
   clasName: z.string().min(1),
   academicYear: z.string().min(4),
 });
